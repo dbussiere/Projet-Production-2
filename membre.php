4537c5c4e2c866e4f7fa04e8aa28e233371932ef
trybalz <sancl.graph@gmail.com>
2017-04-05 09:26:19 -0400

    5 avril

---
+++ membre.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Title Page</title>
+		<script src="https://use.fontawesome.com/663ece6c6a.js"></script>
+		
+		<!-- GOOGLE FONT -->
+		<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/style.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="container-fluid nopnom">
+			<h2 class="eve">ZONE MEMBRE</h2>
+		</div>
+		<div class="container servfree">
+			<h3>SERVICES GRATUITS AUX MEMBRES</h3>
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 servmembre">
+			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 aidecom">
+				<h5>AIDE AUX COMMUNICATIONS</h5>
+				<ul>
+					<li><a href="#"><i class="fa fa-newspaper-o" aria-hidden="true"></i>Tutorat de presse</a></li>
+					<li><a href="#"><i class="fa fa-bullhorn" aria-hidden="true"></i>Diffusion médiatique</a></li>
+					<li><a href="#"><i class="fa fa-microphone" aria-hidden="true"></i>Radio</a></li>
+				</ul>
+			</div>
+			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+				<h5>ESPACES DE RENCONTRE</h5>
+				<ul>
+					<li><a href="#"><i class="fa fa-street-view" aria-hidden="true"></i>Salle de réunion</a></li>
+					<li><a href="#"><i class="fa fa-black-tie" aria-hidden="true"></i>Salon des entrepreuneurs</a></li>
+				</ul>
+			</div>
+			</div>
+		</div>
+
+
+		<div class="container-fluid implication">
+			<h3 class="reseau">S'IMPLIQUER, ÊTRE EN ACTION<br>SAISIR DES OPPORTUNITÉS !</h3>
+			<p class="evetxt">Être membre de la CCIS, c'est choisir de solidifier ses acquis en partageant votre expertise et celle d'autres membres<br>tout en bénéficiant de plusieurs services vous donnant accès à une clientèle privilège.</p>		
+		</div>
+
+		<div class="container-fluid nopnom">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fondgris nopnom">
+				<div class="implications blocrose"><h4>IMPLICATIONS</h4></div>
+				<div class="col-xs-6 col-sm-6 col-md-6 col-md-offset-6 col-lg-6 col-lg-offset-6">
+				<div class="impltxt">
+					<h6 class="bold">Impliquez-vous!</h6>
+					<p>S'impliquer dans un ou des comités d'activités de la CCIS est enrichissantet permet notamment de renforcer votre visibilité dans le réseau. Nos comités sont éthiques et respectent un accord sur les conflits d'intérêts et la confidentialité.</p>
+					<p>Envie de mettre l'épaule à la roue?</p>
+					<a href="#"><button type="button">En savoir plus</button></a>
+				</div>
+				</div>
+			</div>
+
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fondgris reseaumargetop">
+				<div class="reseautage blocrose"><h4>RÉSEAUTAGE</h4></div>
+				<div class="col-xs-6 col-sm-6 col-md-6 col-md-offset-6 col-lg-6 col-lg-offset-6">
+				<div class="resetxt">
+					<h6 class="bold">L'importance du réseau d'affaires !</h6>
+					<p>Améliorer votre réseautage d'affaires et de rencontrer des collaborateurs, fournisseurs et clients potentiels. Venez partager des astuces avec des gens qui comprennent votre réalité! Posséder un bon réseau est une véritable valeur ajoutée à votre entreprise.</p>
+					<p>Consultez-le répertoire des membres:</p>
+					<a href="#"><button type="button"><i class="fa fa-user-plus" aria-hidden="true"></i>Répertoire des membres</button></a>
+				</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container-fluid">
+			<div class="container">
+				<h3 class="membre">UNE BONNE RELATION D'AFFAIRES<br>ÇA COMMENCE ICI!</h3>
+				<p>La Chambre met en valeur le succès économique régional des entreprises et des gens d'affaires de la région. Elle intervient de façon vigilante et constructive auprès des instances et autorités afin de défendre ses membres. Elle stimule le réseautage de ses membres et développe les compétences des gens d'affaires en offrant de l'information, de l'expertise eten les aiguillant vers les ressources locales.</p>
+
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<p><span class="bold dark">Être membre c'est</span><span class="huge bold dark">25% de rabais</span><span class="txt16">sur tous les évènements</span></p>
+					<div class="dadhesion">
+						<p class="titredadh">Double-adhésion !</p>
+						<p>Obtenez le double de services avec la double-adhésion CCIS + Jeune Chambre de commerce de la Mauricie</p>
+						<a href="#" class="txtrouge">jccm.qc.ca</a>
+					</div>
+					<button type="button"><i class="fa fa-user" aria-hidden="true"></i>Devenir membre</button>
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<h4>Gala Distinction Desjardins</h4>
+					<p class="soustgala">Démarquez-vous et soyez reconnu !</p>
+					<p>Chaque année, le Gala rassemble au-delà de 400 personnes impliquées de près ou de loin dans le milieu des affaires. Pour beaucoup, il s'agit d'un accès privilégié à cette communauté et d'un rayonnement incomparable pour l'entreprise.</p>
+					<p class="galadec">Découvrez les avantages d'être finaliste:</p>
+					<button type="button"><i class="fa fa-trophy" aria-hidden="true"></i>Devenez finaliste</button>
+				</div>
+				<div class="logomembre">
+					<img src="img/logoccis.png" alt="">
+				</div>
+			</div>
+
+		</div>
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="/js/blogue.js"></script>
+	</body>
+</html>
