51e4e79b36861f4b3e856e80d5562e2ea43e8599
trybalz <sancl.graph@gmail.com>
2017-04-06 13:44:22 -0400

    Accueil terminé

--- index.php
+++ index.php
@@ -28,35 +28,39 @@
 			<img src="img/slider.png" alt="">
 		</div>
 
-		<div class="container-fluid partmajeur">
+		<div class="container-fluid partmajeur nopnom">
 			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 partmajeurgris">
-				<h2>PARTENAIRES MAJEURS</h2>
+				<h2>PARTENAIRES<br><span class="bold">MAJEURS</span></h2>
 			</div>
 			<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-					<img src="img/shawinigan.png" alt="">
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 accpushshawi">
+					<a href="#"><img src="img/shawinigan.png" alt="" class="acclogo logoshawi"></a>
 				</div>
 				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-					<img src="img/caisse.png" alt="">
+					<a href="#"><img src="img/caisse.png" alt="" class="acclogo logodesjardin"></a>
 				</div>
 				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-					<img src="img/laurentide.png" alt="">
+					<a href="#"><img src="img/laurentide.png" alt="" class="acclogo logolauren"></a>
 				</div>
 				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-					<img src="img/deloitte.png" alt="">
+					<a href="#"><img src="img/deloitte.png" alt="" class="acclogo logodeloi"></a>
 				</div>
 			</div>
 		</div>
 
-		<div class="container">
+		<div class="container accdevenirpartenaire">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 contourgris">
 				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 fondrouge">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 rougetrans">
+						
+					
 					<p>Être partenaire,<br>C'est une valeur ajoutée à votre entreprise !</p>
-					<button type="button">En savoir plus</button>
+					<a href="#"><button type="button">En savoir plus</button></a>
+					</div>
 				</div>
 				<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 parttxt">
-					<h3>Vous désirez partager votre expertise<br>ou vos services en échange d'une grande visibilité?</h3>
-					<p>La CCIS compte parmi ses collaborateurs plus de 25 partenaires qui participent activement au développement économique local par des actions de partage et de soutien. La Chambre s'assure d'offrir une belle visibilité sur toutes ses plates-formes de communication et lors de ses événements en plus de s'assurer d'une étroite collaboration.</p>
+					<h3 class="txtgrisfonce">Vous désirez partager votre expertise<br>ou vos services en échange d'une grande visibilité?</h3>
+					<p class="txtgrispale">La CCIS compte parmi ses collaborateurs plus de 25 partenaires qui participent activement au développement économique local par des actions de partage et de soutien. La Chambre s'assure d'offrir une belle visibilité sur toutes ses plates-formes de communication et lors de ses événements en plus de s'assurer d'une étroite collaboration.</p>
 				</div>
 			</div>
 		</div>
@@ -68,37 +72,37 @@
 		<div class="container">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 acphoto acphoto1">
 				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fdbleu">
-					<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-						<h4>JEUNES ENTREPRENEURS</h4>
+					<div class="col-xs-6 col-sm-6 col-md-6 col-md-offset-6 col-lg-6 col-lg-offset-6">
+						<h4 class="txtblanc bold">JEUNES ENTREPRENEURS</h4>
 					</div>
 				</div>
 				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fdgris">
-					<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-						<h3>LE GOÛT D'ENTREPRENDRE DÈS LE PRÉSCOLAIRE</h3>
-						<p>Chaque année, de nombreuses classes du préscolaire, du primaire et du secondaire réalisent des projets qui visent à résoudre des problèmes ou à répondre à des besoins identifiés par les élèes auprès d'un large public cible.</p>
-						<p class="turquoise"><span class="date">28 mars 2017</span> / par Denis Morin <span class="plus">+</span></p>
+					<div class="col-xs-6 col-sm-6 col-md-6 col-md-offset-6 col-lg-6 col-lg-offset-6">
+						<h3 class="bold txtgrisfonce">LE GOÛT D'ENTREPRENDRE<br>DÈS LE PRÉSCOLAIRE</h3>
+						<p class="txtgrispale">Chaque année, de nombreuses classes du préscolaire, du primaire et du secondaire réalisent des projets qui visent à résoudre des problèmes ou à répondre à des besoins identifiés par les élèes auprès d'un large public cible.</p>
+						<p class="turquoise bdblanc"><span class="date">28 mars 2017</span> / par Denis Morin<span class="plus">+</span></p>
 					</div>
-				</div>
-				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 acimg">
+					<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 acimg">
 					<img src="img/acckid.png" alt="">
+				</div>
 				</div>
 			</div>
 
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 acphoto acphoto1">
-				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fdbleu">
-					<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-						<h4>LES GRANDS DOSSIERS</h4>
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 acphoto acphoto2">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fdbleupale">
+					<div class="col-xs-6 col-sm-6  col-md-6 col-md-offset-6 col-lg-6 col-lg-offset-6">
+						<h4 class="txtblanc bold">LES GRANDS DOSSIERS</h4>
 					</div>
 				</div>
 				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fdgris">
-					<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-						<h3>À L'AFFÛT DES NOUVELLES TECHNOLOGIES</h3>
-						<p>Chaque année, de nombreuses classes du préscolaire, du primaire et du secondaire réalisent des projets qui visent à résoudre des problèmes ou à répondre à des besoins identifiés par les élèes auprès d'un large public cible.</p>
-						<p class="turquoise"><span class="date">28 mars 2017</span> / par Georges Abdul-Nour <span class="plus">+</span></p>
+					<div class="col-xs-6 col-sm-6 col-md-6 col-md-offset-6 col-lg-6 col-lg-offset-6">
+						<h3 class="bold txtgrisfonce">À L'AFFÛT DES NOUVELLES TECHNOLOGIES</h3>
+						<p class="txtgrispale">Chaque année, de nombreuses classes du préscolaire, du primaire et du secondaire réalisent des projets qui visent à résoudre des problèmes ou à répondre à des besoins identifiés par les élèes auprès d'un large public cible.</p>
+						<p class="turquoise bdblanc"><span class="date">28 mars 2017</span> / par Georges Abdul-Nour<span class="plus">+</span></p>
 					</div>
-				</div>
-				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 acimg">
+					<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 acimg">
 					<img src="img/accimg2.png" alt="">
+				</div>
 				</div>
 			</div>
 		</div>
@@ -106,23 +110,23 @@
 		<div class="container-fluid nopnom">
 			<h2 class="actu">ÉVÈNEMENTS</h2>
 		</div>
-		<div class="container">
+		<div class="container acceve">
 			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 acceve1">
-					<h4>DÉJEUNER-PANEL DE DISCUSSION SUR LA MAIN-D'OEUVRE EN TI</h4>
+					<h4 class="bold txt18">DÉJEUNER-PANEL DE DISCUSSION SUR LA MAIN-D'OEUVRE EN TI</h4>
 					<a href="#">	
 						<p class="savoirplus">En savoir plus</p>
 					</a>
 			</div>
 
 			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 acceve2">
-					<h4>DÉFI<br>DES ENTREPRISES</h4>
+					<h4 class="bold">DÉFI<br>DES ENTREPRISES</h4>
 					<a href="#">	
 						<p class="savoirplus">En savoir plus</p>
 					</a>
 			</div>
 
-			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 acceve2">
-					<h4>7E ÉDITION<br>TOUTE LA VILLE PARLE PME</h4>
+			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 acceve3">
+					<h4 class="bold">7E ÉDITION<br>TOUTE LA VILLE PARLE PME</h4>
 					<a href="#">	
 						<p class="savoirplus">En savoir plus</p>
 					</a>
