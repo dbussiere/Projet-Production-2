94dc754a4747bf39d50410f1c7f803c7a9679e90
trybalz <sancl.graph@gmail.com>
2017-04-06 09:37:43 -0400

    Début Accueil

--- index.php
+++ index.php
@@ -21,148 +21,50 @@
 			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 		<![endif]-->
 	</head>
+	
 	<body>
-		<div class="container-fluid nopnom">
-			<h2 class="actu">ACTUALITÉS RÉCENTES</h2>
+		<div class="container-fluid slider nopnom">
+			<!-- Un plugin slider vient ici ! -->
+			<img src="img/slider.png" alt="">
 		</div>
 
-		<div class="container" >
-		<div id="act">
-		<a href="">
-		<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 jentrepreneur">
-
-			<h3>JEUNES ENTREPRENEURS</h3>
-			<img src="https://dummyimage.com/430x300/000/fff" alt="#">
-			<h4>LE GOÛT D'ENTREPRENDRE DÈS LE PRÉSCOLAIRE</h4>
-			<p>Chaque année, de nombreuses classes du préscolaire, du primaire et du secondaire réalisent des projets qui visent à résoudre des problèmes ou à répondre à des besoins identifiés par les élèves auprès d'un large public cible.</p>
-			<p class="turquoise"><span class="date">28 mars 2017</span> / par Denis Morin <span class="plus">+</span></p>
-			
-		</div>
-		</a>
-		</div>
-		<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 techno bordertop nopnom">
-				<h5>TECHNOLOGIES</h5>
-				<a href="#" id="actu1"><p class="gras">RENCORCEMENT DU CRÉNEAU DE L'ÉLECTRIFICATION DES TRANSPORTS À SHAWINIGAN</p></a>
-				<p class="margbot"><span class="date">28 mars 2017</span> / par Denis Morin</p>
+		<div class="container-fluid partmajeur">
+			<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 partmajeurgris">
+				<h2>PARTENAIRES MAJEURS</h2>
 			</div>
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 gdossier bordertop nopnom">
-				<h5>LES GRANDS DOSSIERS</h5>
-				<a href="#" id="actu2"><p class="gras">À L'AFFÛT DES NOUVELLES TECHNOLOGIES</p></a>
-				<p class="margbot"><span class="date">28 février 2017</span> / par Georges Abdul-Nour</p>
-			</div>
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 succes bordertop nopnom">
-				<h5>VOS SUCCÈS D'AFFAIRES</h5>
-				<a href="#" id="actu3"><p class="gras">LE PARC DE L'ILE MELVILLE - SUCCÈS D'AFFAIRES D'ICI !</p></a>
-				<p class="margbot"><span class="date">12 mars 2017</span> / par CCIS</p>
-			</div>
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 entrep bordertop nopnom">
-				<h5>JEUNES ENTREPRENEURS</h5>
-				<a href="#" id="actu4"><p class="gras">À L'AFFÛT DES NOUVELLES TECHNOLOGIES</p></a>
-				<p class="margbot"><span class="date">28 février 2017</span> / par Georges Abdul-Nour</p>
-			</div>
-			<a href="#"><button type="button" class="seeall">section actualités <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></button></a>
-
-
-		</div>
-		</div>
-
-		<div class="container-fluid nopnom">
-			<h2 class="eve">ÉVÈNEMENTS</h2>
-		</div>
-		<div class="container evenement">
-			<h3 class="reseau">POUR UN RÉSEAUTAGE ACTIF !</h3>
-			<p class="evetxt">La CCIS organise plus de 10 activités par année afin de favoriser le réseautage d'affaires entre les gens d'affaires de la région. De plus, nos conférences, lunchs d'affaires ciblés et autres activités peuvent vous permettre d'entrer en contact avec des gens et des entreprises en lien direct avec votre domaine d'affaires.</p>
-			<div class="row">
-				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 event" id="event">
-					<a href="#">
-						<img src="https://dummyimage.com/550x300/000/" alt="#">
-						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ineve">
-							<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-								<p class="dte dteimage"><span class="nbre">02</span>avril</p>
-							</div>
-							<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 txtimg">
-								<p>7E ÉDITION<br>TOUTE LA VILLE PARLE PME</p>
-								<p class="det">détails & inscription</p>
-							</div>
-						</div>
-					</a>
+			<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+					<img src="img/shawinigan.png" alt="">
 				</div>
-				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-
-					<div class="evelist">
-						<p class="dte"><span class="nbre">08</span>avril</p>
-						<a href="#" id="eve1"><p class="evelistdet">DÉJEUNER-PANEL DE DISCUSSION SUR LA MAIN D'OEUVRE EN TI</p>
-						<p class="red">En savoir plus +</p>
-						</a>
-					</div>
-
-					<div class="evelist">
-						<p class="dte"><span class="nbre">12</span>avril</p>
-						<a href="#" id="eve2"><p class="evelistdet">DÉJEUNER-PANEL DE DISCUSSION SUR LA MAIN D'OEUVRE EN TI</p>
-						<p class="red">En savoir plus +</p>
-						</a>
-					</div>
-					<a href="evenement.php"><button type="button" class="seeall seealleve">section événements <i class="fa  fa-arrow-circle-o-right" aria-hidden="true"></i></button></a>
-					
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+					<img src="img/caisse.png" alt="">
+				</div>
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+					<img src="img/laurentide.png" alt="">
+				</div>
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+					<img src="img/deloitte.png" alt="">
 				</div>
 			</div>
 		</div>
-		<div class="container-fluid congris">
-			<div class="container conblanc">
-				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 imgrouge">
-					
-					<a href="#"><div class="rougetrans"><p>Être partenaire,<br>c'est une valeur ajoutée à votre entreprise!</p><p class="savoirplus">En savoir plus</p></div></a>
+
+		<div class="container">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 contourgris">
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 fondrouge">
+					<p>Être partenaire,<br>C'est une valeur ajoutée à votre entreprise !</p>
+					<button type="button">En savoir plus</button>
 				</div>
-				<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 txtconblanc">
+				<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8 parttxt">
 					<h3>Vous désirez partager votre expertise<br>ou vos services en échange d'une grande visibilité?</h3>
 					<p>La CCIS compte parmi ses collaborateurs plus de 25 partenaires qui participent activement au développement économique local par des actions de partage et de soutien. La Chambre s'assure d'offrir une belle visibilité sur toutes ses plates-formes de communication et lors de ses événements en plus de s'assurer d'une étroite collaboration.</p>
 				</div>
 			</div>
 		</div>
+
 		<div class="container-fluid nopnom">
-			<h2 class="formation">FORMATIONS</h2>
+			<h2 class="actu">ACTUALITÉS RÉCENTES</h2>
 		</div>
-		<div class="container evenement">
-			<h3 class="reseau">BONIFIEZ VOTRE EXPERTISE !</h3>
-			<p class="formationtxt">Être membre de la CCIS, c'est choisir de solidifier ses acquis à travers des formations.</p>
-			<p class="formationtxt">Les formations offrent également un lieu d'échange où partager votre expertise avec celle d'autres membres tout en bénéficiant de plusieurs services vous donnant accès à une clientèle privilège.</p>
-
-			<div class="row margetop">
-				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 event" id="formation">
-					<a href="#">
-						<img src="https://dummyimage.com/550x300/000/" alt="#">
-						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ineve">
-							<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-								<p class="dte dteimage"><span class="nbre">07</span>avril</p>
-							</div>
-							<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 txtimg">
-								<p>ÉBAUCHE D'UNE OFFRE<br>DE PARTENARIAT D'AFFAIRES</p>
-								<p class="det">détails & inscription</p>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-					<div class="evelist">
-						<p class="dte"><span class="nbre">11</span>mai</p>
-						<a href="#" id="form1"><p class="evelistdet">LA TENUE DE LIVRE:<br>MÉTHODE SIMPLIFIÉE</p>
-						<p class="red">En savoir plus +</p>
-						</a>
-					</div>
-
-					<div class="evelist">
-						<p class="dte"><span class="nbre">04</span>juin</p>
-						<a href="#" id="form2"><p class="evelistdet">MARKETING WEB:<br>RÉFÉRENCEMENT ET OPTIMISATION</p>
-						<p class="red">En savoir plus +</p>
-						</a>
-					</div>
-					<div class="seealleve">
-					<a href="formation.php"><button type="button" class="seeall seeallform">section formations <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></button></a>
-					</div>
-				</div>
-			</div>
-		</div>
+		
 		<!-- jQuery -->
 		<script src="//code.jquery.com/jquery.js"></script>
 		<!-- Bootstrap JavaScript -->
