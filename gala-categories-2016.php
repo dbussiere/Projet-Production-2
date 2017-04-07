3cd3b04457a75edfc4c11aeb9eec75a961293529
Dave <dave_bussiere@hotmail.com>
2017-04-07 16:41:18 -0400

    DONE // page Catégories 2016 (Gala)
    DONE // page Catégories 2016 (Gala)
    
    Voir fichiers : gala-categories-2016.php + gala-categories.css
    
    - intégration statique, OK
    - fullscreen, OK
    - responsive, OK

---
+++ gala-categories-2016.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>CATÉGORIES ÉDITIONS PRÉCÉDENTES</title>
+		<script src="https://use.fontawesome.com/663ece6c6a.js"></script>
+		
+		<!-- GOOGLE FONT -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/gala-categories.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+        <!-- Top bar Section Catégories Édition Précédentes -->
+		<div class="container-fluid no-padding no-margin">
+
+            <!-- Top Bar -->
+			<h2 class="top-bar-section gala-categories">Catégories éditions précédentes</h2>
+
+            <!-- Title Finaliste -->
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 title-gala-categories">
+                <h3>Gala 2016</h3>
+            </div>
+
+		</div>
+
+        <!-- Contenu Section Catégories Gala-->
+		<div class="container no-padding">
+
+            <!-- Contenu -->
+            <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 section-categories no-padding">
+
+                <div class="row">
+
+                    <!-- Édition Title -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 info-categories no-padding">
+                        <h2>Catégories</h2>
+                        <h3>23e Édition Gala Distinction Desjardins</h3>
+                    </div>
+                    
+                    <!-- Catégories -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        
+                        <!-- Left Part -->
+                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 bloc-left-part">
+
+                            <!-- Single bloc categorie -->
+                            <h4>Commerce de détail et restauration :</h4>
+                            <p>Ce prix est décerné à une entreprise commerciale ou de restauration répondant aux besoins de la clientèle, qui s’adapte à la concurrence, qui innove dans sa gamme de produits ou de services, qui entretient son espace et y investit, qui s’implique dans l’animation ou le dynamisme de son milieu. Sont admissibles les entreprises offrant des services de vente au détail destinés au grand public à partir de points de vente établis, incluant les entreprises de restauration, mais excluant les entreprises de services publics et les organismes sociaux.</p>
+
+                            <!-- Single bloc categorie -->
+                            <h4>Tourisme et hébergement :</h4>
+                            <p>Une entreprise, un organisme ou un événement ayant favorisé l’accroissement des recettes touristiques et le positionnement accru de la ville de Shawinigan. Une attention particulière est accordée aux initiatives ayant favorisé l’attraction du territoire en lui associant un événement de tourisme d'affaires, un festival, un événement sportif ou culturel susceptible d’attirer ou de prolonger le séjour des visiteurs.</p>
+
+                            <!-- Single bloc categorie -->
+                            <h4>Employeur de choix :</h4>
+                            <p>Le prix Employeur de choix récompense tout type d’entreprise qui s’est démarquée par une ou des pratiques de gestion des ressources humaines qu’elle a mises en place. Entre autres, il peut s’agir de pratiques en matière de conciliation travail-vie personnelle, d’environnement de travail, de formation, de développement et d’amélioration des compétences de ses employés, de méthodes innovantes relativement à la rétention ou à l’attraction de la main-d’œuvre, etc.</p>
+
+                            <!-- Single bloc categorie -->
+                            <h4>Innovation et nouveaux marchés :</h4>
+                            <p>Une entreprise ou un organisme qui se distingue par la conquête de nouveaux marchés ou de nouveaux territoires (provincial, national ou international), le développement de nouveaux procédés, d’un produit ou d’un service innovateur, de nouvelles techniques de commercialisation et qui démontre la capacité de poursuivre son action dans ce domaine ou dans l’adaptation et l’application de technologies nouvelles.</p>
+
+                            <!-- Single bloc categorie -->
+                            <h4>Nouvelle entreprise :</h4>
+                            <p>Une entreprise qui est en opération depuis au moins six (6) mois, pour un maximum de 36 mois. Elle se démarque par l’évolution de sa rentabilité, ses réalisations exceptionnelles ainsi que la qualité de sa gestion. L’entreprise démontre des perspectives de développement à long terme.</p>
+
+                            <!-- Single bloc categorie -->
+                            <h4>Entreprise manufacturière :</h4>
+                            <p>Une entreprise manufacturière qui se distingue par l’excellence de ses produits, ses investissements, son innovation ou le développement de nouveaux marchés, ainsi que par la qualité de gestion de ses procédés.</p>
+
+                            <!-- Single bloc categorie -->
+                            <h4>Entreprise de service aux particuliers :</h4>
+                            <p>Une entreprise qui se démarque par son approche client, son unicité, sa créativité et la qualité des services qu’elle offre aux particuliers.</p>
+
+                            <!-- Single bloc categorie -->
+                            <h4>Entreprise de service aux entreprises :</h4>
+                            <p>Une entreprise qui se démarque par son approche client, son unicité, sa créativité et la qualité des services qu’elle offre aux entreprises.</p>
+
+                        </div><!-- /.Left Part -->
+
+                        <!-- Right Part -->
+                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 bloc-right-part">
+                            
+                            <!-- Single bloc categorie -->
+                            <h4>Économie sociale :</h4>
+                            <p>Catégorie discrétionnaire d’année en année. Un organisme à but non lucratif (OBNL) ayant des activités marchandes ou une coopérative offrant des biens ou services qui respecte les principes de fonctionnement suivants : un processus de gestion démocratique, la participation et la prise en charge individuelle et collective, la primauté de la personne et du travail sur le capital, et finalement, la propriété collective des capitaux et des moyens de production.</p>
+
+                            <!-- Single bloc categorie -->
+                            <h4>Relève entrepreneuriale :</h4>
+                            <p>Catégorie discrétionnaire d’année en année. Entreprise ayant assuré sa pérennité grâce à un processus de transfert, de rachat, de fusion, d’acquisition ou de relève, et ce, depuis au plus deux (2) ans.</p>
+
+                            <!-- Single bloc categorie -->
+                            <h4>Personnalité d'affaires féminine :</h4>
+                            <p class="ul-minus-margin-bottom">La personnalité d’affaires féminine se distingue par ses compétences entrepreneuriales et ses réalisations dans son secteur d’activités, son dynamisme et son engagement social et dans son milieu. La personnalité d’affaires féminine est choisie en fonction des éléments suivants :</p>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Notoriété</li>
+                                    <li>Leadership ou entrepreneurship</li>
+                                    <li>Audace, ténacité, vision</li>
+                                    <li>Engagement au sein de la communauté</li>
+                                </ul>
+
+                            <!-- Single bloc categorie -->
+                            <h4>Personnalité d'affaires masculine :</h4>
+                            <p class="ul-minus-margin-bottom">La personnalité d’affaires masculine se distingue par ses compétences entrepreneuriales et ses réalisations dans son secteur d’activités, son dynamisme et son engagement social et dans son milieu. La personnalité d’affaires masculine est choisie en fonction des éléments suivants :</p>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Notoriété</li>
+                                    <li>Leadership ou entrepreneurship</li>
+                                    <li>Audace, ténacité, vision</li>
+                                    <li>Engagement au sein de la communauté</li>
+                                </ul>
+
+                            <!-- Single bloc categorie -->
+                            <h4>Diversification économique :</h4>
+                            <p>Cette reconnaissance spéciale et publique met en lumière la contribution exceptionnelle d’entreprises de Shawinigan qui ont contribué aux efforts de diversification de l’économie de la ville, soit par son développement ou dans le contexte de démarrage d’une entreprise ou d’un nouveau projet. Cette reconnaissance est décernée par le Comité de diversification et de développement économique de la Ville de Shawinigan.</p>
+
+                            <!-- Single bloc categorie -->
+                            <h4>Prix du jury :</h4>
+                            <p>Catégorie discrétionnaire d’année en année. Ce prix est décerné par le jury de sélection à une entreprise ou un entrepreneur s’étant distingué par l’excellence générale de ses performances ou par un succès exceptionnellement méritoire.</p>
+
+                            <!-- Single bloc categorie -->
+                            <h4>Prix Coup de coeur du public :</h4>
+                            <p>Parmi les entreprises lauréates de la soirée, les participants du Gala distinction Desjardins seront appelés à sélectionner l’entreprise s’étant distinguée par l’excellence générale de ses performances ou par un succès exceptionnellement méritoire. (Les personnalités d’affaires ne sont pas admissibles.)</p>                                                                                                                
+
+                        </div><!-- /.Right Part -->
+
+                    </div><!-- /.Catégories -->
+                </div><!-- /.row -->
+            </section><!-- /.section-categories -->
+		</div><!-- /.container -->
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="/js/blogue.js"></script>
+	</body>
+</html>
