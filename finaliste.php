5c5e1fa2f9074493bda4799ce87f492ddd53f4ad
Dave <dave_bussiere@hotmail.com>
2017-04-07 16:36:47 -0400

    correction mineures //
    correction mineures //
    
    Correction du title dans le code / finaliste.php

--- finaliste.php
+++ finaliste.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>SERVICES GRATUITS AUX MEMBRES</title>
+        <title>PERSONNALITÉ FÉMININE 2017</title>
 		<script src="https://use.fontawesome.com/663ece6c6a.js"></script>
 		
 		<!-- GOOGLE FONT -->
