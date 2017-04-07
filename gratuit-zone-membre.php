a4de40e124797edc2bd0a7df32c0977f04aa4644
Dave <dave_bussiere@hotmail.com>
2017-04-07 10:24:56 -0400

    DONE // aide aux comm. + espaces rencontres
    DONE // aide aux comm. + espaces rencontres
    
    Voir fichier : gratuit-zone-membre.php + gratuit-zone-membre-style.css
    
    - intégration statique, OK
    - fullscreen, OK
    - responsive, OK

---
+++ gratuit-zone-membre.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>SERVICES GRATUITS AUX MEMBRES</title>
+		<script src="https://use.fontawesome.com/663ece6c6a.js"></script>
+		
+		<!-- GOOGLE FONT -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/gratuit-zone-membre.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+        <!-- Top bar Section Article -->
+		<div class="container-fluid no-padding no-margin">
+            
+            <!-- Top Bar -->
+			<h2 class="top-bar-section zone-membre">Zone membre</h2>
+
+            <!-- Title Services Gratuits -->
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 title-services-gratuits">
+                <h3>Services gratuits aux membres</h3>
+            </div>
+
+		</div>
+
+        <!-- Contenu Section Article -->
+		<div class="container no-padding">
+
+            <!-- Blocs Communication + Espaces -->
+            <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 section-services-gratuits">
+                
+                <!-- Aide aux Communication -->
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 aide-communication">
+
+                    <!-- title -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <h3>Aide aux communications</h3>
+                    </div>
+
+                    <!-- Tutorat -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 single-bloc-info">
+                        
+                        <!-- Icon + Tutorat de presse -->
+                        <h4><i class="fa fa-newspaper-o" aria-hidden="true"></i>Tutorat de presse</h4>
+                        <p>Vous désirez faire un lancement d’entreprise officiel et marquant? Vous souhaitez faire l’annonce de votre agrandissement, investissement, acquisition? Rien de mieux qu’une conférence de presse !</p>
+                       
+                        <!-- Bullet list -->
+                        <h5>Contactez-nous et nous veillerons à :</h5>
+                        <ul>
+                            <li>Inviter les médias et les instances politiques</li>
+                            <li>Diffuser l’invitation et l’information sur notre réseau</li>
+                            <li>Rédiger le communiqué de presse</li>
+                            <li>Faire office de maître de cérémonie et animer le protocole</li>
+                        </ul>
+                    </div>
+
+                    <!-- Diffusion Médiatique -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 single-bloc-info">
+                        <h4><i class="fa fa-bullhorn" aria-hidden="true"></i>Diffusion médiatique</h4>
+                        <p>Vous avez simplement un communiqué de presse à faire passer ou une annonce importante? Vous avez gagné un prix, une distinction? Nous souhaitons aussi le partager à nos membres! Vous pourriez même apparaître dans nos « succès d’affaires d’ici ».</p>
+                    </div>
+
+                    <!-- Radio -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <h4><i class="fa fa-microphone" aria-hidden="true"></i>Radio - <em>Succès d’affaires d’ici !</em></h4>
+                        <p>La CCIS souhaite faire connaître les bons coups et les réussites de ses membres. Deux fois par mois, sur les ondes du 92,9 CFUT nous mettons en valeur ces bons coups dans nos capsules radio «Succès d'affaires d'ici». D'une durée de 30 secondes, ces capsules sont diffusées trois fois par jour aux heures de grande écoute. Une belle occasion de visibilité! Faites-nous connaître vos bons coups et vous pourriez les entendre à la radio!</p>
+                    </div>
+                    
+                </div><!-- /.aide-communication -->
+
+                <!-- Espace de Rencontres -->
+                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 espace-rencontre">
+
+                    <!-- title -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <h3>Espaces de rencontres</h3>
+                    </div>
+                    
+                    <!-- Salle de réunion -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 single-bloc-info">
+                        <h4><i class="fa fa-street-view" aria-hidden="true"></i>Salle de réunion</h4>
+                        <p>Pouvant accueillir 8 personnes, notre salle de réunion toute équipée est disponible pour vos réunions professionnelles! Nous faisons même le café ! Contactez-nous pour informations ou réservations.</p>
+                    </div>
+
+                    <!-- Salon des entrepreneurs -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 single-bloc-info">
+                        <h4><i class="fa fa-black-tie" aria-hidden="true"></i>Salon des entrepreneurs</h4>
+                        <p>Idéal pour les entrevues médias, les conférences de presse et les 5@7, notre salon des entrepreneurs est à la disposition des membres. Contactez-nous pour informations ou réservations.</p>
+                    </div>
+
+                    <!-- Inscription membre + info CCIS -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding buttons-contact-ccis">
+                        <ul class="no-padding">
+                            <li class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><a href="" class="inscription-membre-ccis"><i class="fa fa-user" aria-hidden="true"></i><span>Oui</span>, je m'inscris maintenant</a></li>
+                            <li class="col-xs-12 col-sm-12 col-md-12 col-lg-12"><a href="" class="info-ccis"><i class="fa fa-envelope" aria-hidden="true"></i>info@ccishawinigan.ca</a></li>
+                        </ul>
+                    </div>
+
+                </div><!-- /.espace-rencontre -->
+                
+            </section><!-- /.section-services-gratuits -->
+		</div><!-- /.container -->
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="/js/blogue.js"></script>
+	</body>
+</html>
