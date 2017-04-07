49c59839c58c53fb980c6a9e5ff615cd983b75e6
Dave <dave_bussiere@hotmail.com>
2017-04-07 14:15:50 -0400

    DONE // page Finaliste (Madames)
    DONE // page Finaliste (Madames)
    
    Voir fichiers : finaliste.php + finaliste.css
    
    - intégration statique, OK
    - fullscreen, OK
    - responsive, OK

---
+++ finaliste.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>SERVICES GRATUITS AUX MEMBRES</title>
+		<script src="https://use.fontawesome.com/663ece6c6a.js"></script>
+		
+		<!-- GOOGLE FONT -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/finaliste.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+        <!-- Top bar Section Finaliste -->
+		<div class="container-fluid no-padding no-margin">
+
+            <!-- Top Bar -->
+			<h2 class="top-bar-section finaliste">Personnalité d'affaires féminine</h2>
+
+            <!-- Title Finaliste -->
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 title-finaliste-feminine">
+                <h3>Finaliste 2017</h3>
+            </div>
+
+		</div>
+
+        <!-- Contenu Section Finaliste-->
+		<div class="container no-padding">
+
+            <!-- Contenu -->
+            <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 section-finaliste no-padding">
+
+                <div class="row">
+
+                    <!-- Nom Finaliste -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 info-finaliste no-padding">
+                        <h2>Jeanne Fortin</h2>
+                        <h3>Finaliste 2017 - Personnalité d'affaires féminine</h3>
+                        <p>Texte par Sylvie Fortin</p>
+                    </div>
+                    
+                    <!-- Image finaliste + text biographie -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        
+                        <!-- image finaliste -->
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 finaliste-img no-padding no-margin">
+                            <img src="img/finaliste.png" alt="" class="img-responsive">
+                        </div>
+
+                        <!-- Text biograhie -->
+                        <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 bloc-biographie">
+                            <h4>Biographie</h4>
+                            <p>Marie-Jeanne Simon Fortin est née le 7 août 1931 à Edmonton en Alberta. À l’âge de 10 ans, elle revient s’installer au Québec avec sa famille. À l’âge de 22 ans, elle épouse Gaston Fortin. Son mari devant voyager un peu partout en province pour son travail, elle passe beaucoup de temps seule avec ses enfants. Pour meubler ces longues périodes, elle décide d’ouvrir son propre restaurant. En 1965, alors mère de 2 enfants, elle inaugure son restaurant, l’Oiseau Bleu.</p>
+                            <p>Rapidement devant le succès de l’entreprise, son mari se joint au projet ce qui lui permet de cesser de voyager. Ambitieux, le couple Fortin construit un motel près du restaurant qui héberge les travailleurs ainsi que les voyageurs. Pendant la construction du motel, Jeanne donne naissance à sa troisième fille, Sylvie et à son garçon Guy en 1967.</p>
+                            <p>En plus de s’occuper de la gestion du restaurant et du motel et de se consacrer à sa famille, Jeanne Fortin ne s’assoit pas sur ses lauriers. Elle souhaite tenir ses connaissances à jour afin de permettre à l’entreprise de prospérer et d’évoluer. Elle suit donc différents cours: cours de chef cuisinier, service de boisson, gestion d’entreprise, tenue de livres, promotion et de marketing.</p>
+                            <p>Jeanne Fortin et son mari continuent de voir grand. En 1980, ils se portent acquéreur d’un hôtel en faillite afin d’y ouvrir une salle de réception. Jeanne Fortin met sur pied un nouveau service de traiteur. Des centaines de mariages et de réceptions se déroulent dans leur nouveau commerce au cours des années 1980.</p>
+                            <p>Le talent de Jeanne n’était plus à prouver, les gens venaient de partout au Québec afin de déguster l’un de ses plats. Des clientes lui demandent même des cours de cuisine. Elle propose donc un projet au service de l’éducation aux adultes de la Commission scolaire régionale de la Mauricie, et elle a eu le feu vert. Une nouvelle aventure commence. Jeanne Fortin donne jusqu’à 9 cours de cuisine par semaine à plus de 150 élèves.</p>
+                            <p>Femme d’affaires, elle savait gérer ses entreprises en plus de donner tous ses cours. Elle écrivait également des chroniques culinaires « La Bonne Bouffe » dans l’Hebdo du Saint-Maurice. Membre de l’amicale des cuisiniers de la Mauricie, elle a remporté 4 prix au Salon culinaire. Sans parler des nombreux pris d’horticulture, sa deuxième passion. Elle s’est mérité le prix de maison Fleuries pendant plusieurs années.</p>
+                            <p>Jeanne Fortin n’a pas peur de l’aventure, elle a aussi fait plusieurs voyages afin d’en apprendre davantage sur sa passion, la cuisine. En France, elle a suivi des cours de cuisine avec de grands chefs.</p>
+                            <p>Malgré une vie professionnelle très chargée, Jeanne Fortin a néanmoins consacré une partie de son temps à diverses causes de la communauté. Elle s’est engagée dans sa communauté que ce soit dans les comités de parents, comme commissaire d’école, membre de l’AFEAS, des Dames Auxiliaires à l’Hôpital Laflèche, du Groupe de prévention des chutes, présidente de l’AFEAS, cofondatrice des cuisines collectives à Shawinigan et Grand-Mère. Durant 6 ans, elle a également consacré 3 mois par année à des projets humanitaires au Guatemala et ailleurs pour aider des orphelins et préparer des repas pour les plus démunis.</p>
+                            <p>Jeanne Fortin a également été intronisée membre de L’Ordre de la Légion d’Honneur de l’Association Canado-Américaine. Un prix soulignant les efforts de certains individus dans les domaines fraternel, éducationnel ou culturel.</p>
+                            <p>Décédée en septembre 2008, elle a continué son œuvre de bienfaisance en donnant ses organes, ce qui a permis de sauver deux vies. Depuis 2016, son nom est maintenant attribué à une rue de Shawinigan.</p>
+                        </div>
+                        
+                    </div><!-- /.Image finaliste + text biographie -->
+
+                </div><!-- /.row -->
+
+                <div class="row">
+
+                    <!-- Vote -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bloc-vote-finaliste">
+                        <!-- Bouton vote -->
+                        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-lg-offset-6">
+                            <a href="" class="btn-vote-finaliste">Votez pour cette candidate</a>                            
+                        </div>
+                    </div><!-- /.Vote -->
+
+                </div><!-- /.row -->
+            </section><!-- /.section-services-gratuits -->
+		</div><!-- /.container -->
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="/js/blogue.js"></script>
+	</body>
+</html>
