5e76ea83118c048b15b902a89728d3c24d0894d1
trybalz <sancl.graph@gmail.com>
2017-04-07 17:31:10 -0400

    Début gallerie

---
+++ gallerie.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Title Page</title>
+		<script src="https://use.fontawesome.com/663ece6c6a.js"></script>
+		
+		<!-- GOOGLE FONT -->
+		<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/style.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="container-fluid nopnom">
+			<h2 class="eve">GALERIE</h2>
+		</div>
+		<div class="container evenement">
+			<h3 class="reseau">DÉCOUVREZ LA CCIS SOUS UN ANGLE DIFFÉRENT !</h3>
+			<p class="evetxt">Que ce soit lors de gala, cocktails, sorties publiques, conférences de presses ou autres évènements le visage de la CCIS et ses membres dégage un dynamisme et une passion contagieuse !<br>Découvrez-le ici!</p>
+		</div>
+
+		<div class="container-fluid nopnom">
+			<h2 class="eve evebleu">ALBUMS PHOTOS</h2>
+		</div>
+
+		<div class="container-fluid plugingallerie">
+			<p>JE SUIS UN PLUGIN GALLERIE PHOTO !!</p>
+		</div>
+		
+
+		<div class="container-fluid nopnom">
+			<h2 class="eve evebrun">VIDÉOS</h2>
+		</div>
+
+		<div class="container-fluid plugingallerie">
+			<p>JE SUIS UN PLUGIN GALLERIE VIDEOS !!</p>
+		</div>
+
+		<div class="container">
+			<div class="relation">
+				<h3 class="membre">UNE BONNE RELATION D'AFFAIRES<br>ÇA COMMENCE ICI!</h3>
+				<p>La Chambre met en valeur le succès économique régional des entreprises et des gens d'affaires de la région. Elle intervient de façon vigilante et constructive auprès des instances et autorités afin de défendre ses membres. Elle stimule le réseautage de ses membres et développe les compétences des gens d'affaires en offrant de l'information, de l'expertise eten les aiguillant vers les ressources locales.</p>
+				</div>
+
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 servmembre txtcentrer logomembrebas">
+		
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 padding7 txtgrispale blocbas">
+					<button type="button"><i class="fa fa-user" aria-hidden="true"></i>Contactez-nous</button>
+					<p><a href="#" class="txt30 bold txtgrisfonce">819 536-0777</a></p>
+					<p><a href="#" class="txt18 txtgrisfonce">info@ccishawinigan.ca</a></p>
+					<p class="txt16 txtgrisfonce">1250, avenue de la Station, bureau 1-116<br>Shawinigan (Québec)  G9N 1W8</p>
+					
+				</div>
+				
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 txtgrispale padding7 blocbas2">
+					<button type="button"><i class="fa fa-user" aria-hidden="true"></i>Devenir membre</button>
+					<p class="txtgrisfonce txt18 sousgala">Un réseau de plus de</p>
+					<p class="txt30 txtgrisfonce gala">600 gens d'affaires</p>	
+					<button type="button"><i class="fa fa-trophy" aria-hidden="true"></i>Devenez finaliste</button>
+				</div>
+				<div class="logomembre">
+					<img src="img/logoccis.png" alt="">
+				</div>
+				</div>
+		</div>
+
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="/js/blogue.js"></script>
+	</body>
+</html>
