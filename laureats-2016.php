03282e068b6f64fa6846192c59c46cf2dc77a447
Dave <dave_bussiere@hotmail.com>
2017-04-07 17:47:15 -0400

    DONE // page Lauréats 2016 (Gala)
    DONE // page Lauréats 2016 (Gala)
    
    Voir fichiers : laureats-2016.php + laureats.css
    
    - intégration statique, OK
    - fullscreen, OK
    - responsive, OK

---
+++ laureats-2016.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>LAURÉATS - 2016</title>
+		<script src="https://use.fontawesome.com/663ece6c6a.js"></script>
+		
+		<!-- GOOGLE FONT -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/laureats.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+        <!-- Top bar Section Lauréats Édition Précédentes -->
+		<div class="container-fluid no-padding no-margin">
+
+            <!-- Top Bar -->
+			<h2 class="top-bar-section gala-laureats">Lauréats éditions précédentes</h2>
+
+            <!-- Title Lauréats -->
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 title-gala-laureats">
+                <h3>Gala 2016</h3>
+            </div>
+
+		</div>
+
+        <!-- Contenu Section Lauréats Gala-->
+		<div class="container no-padding">
+
+            <!-- Contenu -->
+            <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 section-laureats no-padding">
+
+                <div class="row">
+
+                    <!-- Édition Title -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 info-laureats no-padding">
+                        <h2>Gagnants & Finalistes</h2>
+                        <h3>23e Édition Gala Distinction Desjardins</h3>
+                    </div>
+                    
+                    <!-- Lauréats -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        
+                        <!-- Left Part -->
+                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 bloc-left-part">
+
+                            <!-- Single bloc Lauréat -->
+                            <h4>Commerce de détail et restauration :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Canadian Tire - Entreprises Sylvie Drolet - <span>Gagnants</span></li>
+                                    <li>Sensée épicerie spécialisée</li>
+                                    <li>Boucherie Nobert Inc.</li>
+                                </ul>
+
+                            <!-- Single bloc Lauréat -->
+                            <h4>Tourisme et hébergement :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Défis du Parc - <span>Gagnants</span></li>
+                                    <li>Soirée des Brasseurs</li>
+                                    <li>Auberge Gouverneur Shawinigan</li>
+                                </ul>
+
+                            <!-- Single bloc Lauréat -->
+                            <h4>Employeur de choix :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Synapse Électronique - <span>Gagnants</span></li>
+                                    <li>Peinture Robert Dupont Inc.</li>
+                                    <li>IGA Extra Famille Baril</li>
+                                </ul>
+
+                            <!-- Single bloc Lauréat -->
+                            <h4>Innovation et nouveaux marchés :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Conseillers en gestion et informatique CGI Inc. - <span>Gagnants</span></li>
+                                    <li>ICO Technologies Inc.</li>
+                                </ul>
+
+                            <!-- Single bloc Lauréat -->
+                            <h4>Nouvelle entreprise :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Fait par une maman - <span>Gagnants</span></li>
+                                    <li>Ora Boutique</li>
+                                    <li>Coffrage Pelletier</li>
+                                </ul>
+
+                            <!-- Single bloc Lauréat -->
+                            <h4>Entreprise manufacturière :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Synapse Électronique - <span>Gagnants</span></li>
+                                    <li>Shawinigan Aluminium</li>
+                                </ul>
+
+                            <!-- Single bloc Lauréat -->
+                            <h4>Entreprise de service aux particuliers :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Pépinière du Parc - <span>Gagnants</span></li>
+                                    <li>Location et service Patrice Raiche mini-mécanique Inc.</li>
+                                </ul>
+
+                        </div><!-- /.Left Part -->
+
+                        <!-- Right Part -->
+                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 bloc-right-part">
+                            
+                            <!-- Single bloc Lauréat -->
+                            <h4>Entreprise de service aux entreprises :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Val-Mauricie électrique - <span>Gagnants</span></li>
+                                    <li>Transport Chaîné Inc.</li>
+                                    <li>Comméléo Inc.</li>
+                                </ul>
+
+                            <!-- Single bloc Lauréat -->
+                            <h4>Économie sociale :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>92,9 CFUT - <span>Gagnants</span></li>
+                                    <li>La Corporation TGV Net Mauricie</li>
+                                </ul>
+
+                            <!-- Single bloc Lauréat -->
+                            <h4>Relève entrepreneuriale :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>ICO Technologies Inc. - <span>Gagnants</span></li>
+                                    <li>Fix-auto Shawinigan-Sud</li>
+                                </ul>
+
+                            <!-- Single bloc Lauréat -->
+                            <h4>Personnalité d'affaires féminine :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Marie-Josée Gervais - <span>Gagnants</span></li>
+                                </ul>
+
+                            <!-- Single bloc Lauréat -->
+                            <h4>Personnalité d'affaires masculine :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Roger Lavergne- <span>Gagnants</span></li>
+                                </ul>
+
+                            <!-- Single bloc Lauréat -->
+                            <h4>Prix du jury :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Sensée épicerie spécialisée - <span>Gagnants</span></li>
+                                </ul>
+
+                            <!-- Single bloc Lauréat -->
+                            <h4>Diversification économique :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Sim-Cognibox - <span>Gagnants</span></li>
+                                    <li>AddÉnergie - <span>Gagnants</span></li>
+                                    <li>Comaintel Inc. - <span>Gagnants</span></li>
+                                </ul>
+
+                            <!-- Single bloc Lauréat -->
+                            <h4>Prix Coup de coeur du public :</h4>
+
+                                <!-- Bullet list -->
+                                <ul>
+                                    <li>Comméléo Inc. - <span>Gagnants</span></li>
+                                </ul>
+
+                        </div><!-- /.Right Part -->
+
+                    </div><!-- /.Lauréats -->
+                </div><!-- /.row -->
+            </section><!-- /.section-lauréats -->
+		</div><!-- /.container -->
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="/js/blogue.js"></script>
+	</body>
+</html>
