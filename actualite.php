99f27264ac6004448e9175462ac5196865850679
Dave <dave_bussiere@hotmail.com>
2017-04-08 17:02:37 -0400

    DONE // page Actualité (À la une)
    DONE // page Actualité (À la une)
    
    Voir fichiers : actualite.php + actualite-style.css
    
    - intégration statique, OK
    - fullscreen, OK
    - responsive, OK

---
+++ actualite.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>ACTUALITÉS - À LA UNE</title>
+		<script src="https://use.fontawesome.com/663ece6c6a.js"></script>
+		
+		<!-- GOOGLE FONT -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/actualite-style.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+        <!-- Top bar Section Article -->
+		<div class="container-fluid no-padding no-margin">
+			<h2 class="top-bar-section actualite">À la une</h2>
+		</div>
+
+        <!-- Contenu Section Article -->
+		<div class="container no-padding">
+
+            <!-- Block article -->
+            <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 section-article no-padding no-margin">
+
+                <!-- Bloc Single Article -->
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 bloc-article no-padding no-margin">
+
+                    <!-- Article -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 article">
+
+                        <div class="row">
+
+                            <!-- Titre article -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 title-article-category">
+                                <h3 class="col-xs-12 col-sm-12 col-md-12 col-lg-12">Jeunes entrepreneurs</h3>
+                            </div>
+
+                            <!-- Image Article -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+                                <!-- image -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 single-article-img no-padding">
+                                    <img src="img/actualite-article-img-example.png" alt="" class="img-responsive">
+                                </div>
+
+                            </div>
+
+                        </div><!-- /.row -->
+                        
+                        <div class="row">
+
+                            <!-- Content article -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 content-article">
+
+                                <!-- title + author -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 title-article no-padding no-margin">
+                                    
+                                    <!-- Title Link -->
+                                    <a href="#" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">Le goût d'entreprendre dès le préscolaire</a>
+                                    
+                                    <!-- Bloc Extrait -->
+                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-article">
+
+                                        <!-- Extrait -->
+                                        <p>Chaque année, de nombreuses classes du préscolaire, du primaire et du secondaire réalisent des projets qui visent à résoudre des problèmes ou à répondre à des besoins identifiés par les élèves auprès d’un large public cible...</p>
+
+                                    </div>
+
+                                    <!-- Author -->
+                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-7 author">
+                                        <p class="category-color"><a href="#"><span class="date-article">28 mars 2017</span> / par Denis Morin <span class="plus-article pull-right">+</span></a></p>
+                                    </div>                                    
+
+                                </div>
+                            </div><!-- /.Content article -->
+
+                        </div><!-- /.row -->
+                    </div><!-- /.Article -->
+                </div><!-- /.bloc-article -->
+
+                <!-- Bloc Single Article -->
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 bloc-article no-padding no-margin">
+
+                    <!-- Article -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 article">
+
+                        <div class="row">
+
+                            <!-- Titre article -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 title-article-category">
+                                <h3 class="col-xs-12 col-sm-12 col-md-12 col-lg-12">Jeunes entrepreneurs</h3>
+                            </div>
+
+                            <!-- Image Article -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+                                <!-- image -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 single-article-img no-padding">
+                                    <img src="img/actualite-article-img-example.png" alt="" class="img-responsive">
+                                </div>
+
+                            </div>
+
+                        </div><!-- /.row -->
+                        
+                        <div class="row">
+
+                            <!-- Content article -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 content-article">
+
+                                <!-- title + author -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 title-article no-padding no-margin">
+                                    
+                                    <!-- Title Link -->
+                                    <a href="#" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">Le goût d'entreprendre dès le préscolaire</a>
+                                    
+                                    <!-- Bloc Extrait -->
+                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-article">
+
+                                        <!-- Extrait -->
+                                        <p>Chaque année, de nombreuses classes du préscolaire, du primaire et du secondaire réalisent des projets qui visent à résoudre des problèmes ou à répondre à des besoins identifiés par les élèves auprès d’un large public cible...</p>
+
+                                    </div>
+
+                                    <!-- Author -->
+                                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-7 author">
+                                        <p class="category-color"><a href="#"><span class="date-article">28 mars 2017</span> / par Denis Morin <span class="plus-article pull-right">+</span></a></p>
+                                    </div>                                    
+
+                                </div>
+                            </div><!-- /.Content article -->
+
+                        </div><!-- /.row -->
+                    </div><!-- /.Article -->
+                </div><!-- /.bloc-article -->
+
+                <!-- Arrows Section Catégories -->
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 arrows-bar-article">
+                        <i class="col-xs-6 col-sm-6 col-md-6 col-lg-6 fa fa-arrow-circle-o-left" aria-hidden="true"></i>
+                        <i class="col-xs-6 col-sm-6 col-md-6 col-lg-6 fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+                    </div>
+                </div>
+
+            </section><!-- /.section-article -->
+		</div><!-- /.container -->
+
+        <!-- Top bar Section Catégories -->
+		<div class="container-fluid no-padding no-margin">
+			<h2 class="top-bar-section categorie">Naviguez par sujets</h2>
+		</div>
+
+        <!-- Contenu Section Catégories -->
+		<div class="container no-padding">
+
+            <!-- Block Catégories -->
+            <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding">
+
+                <div class="row">
+
+                    <!-- Menu catégories -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 nav-categories no-padding">
+                        <ul class="no-padding">
+                            <li class="col-xs-12 col-sm-6 col-md-6 col-lg-4"><a href="" class="category-technologies">Technologies</a></li>
+                            <li class="col-xs-12 col-sm-6 col-md-6 col-lg-4"><a href="" class="category-jeunes-entrepreneurs">Jeunes entrepreneurs</a></li>
+                            <li class="col-xs-12 col-sm-6 col-md-6 col-lg-4"><a href="" class="category-grands-dossiers">Les grands dossiers</a></li>
+                            <li class="col-xs-12 col-sm-6 col-md-6 col-lg-4"><a href="" class="category-industriel">Industriel</a></li>
+                            <li class="col-xs-12 col-sm-6 col-md-6 col-lg-4"><a href="" class="category-finances">Finances</a></li>
+                            <li class="col-xs-12 col-sm-6 col-md-6 col-lg-4"><a href="" class="category-succes-affaires">Vos succès d'affaires</a></li>
+                        </ul>
+                    </div>
+
+                </div><!-- /.row -->
+
+                <div class="row">
+
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding article-categorie">
+                        <!-- SINGLE block article connexe -->
+                        <div class="col-xs-10 col-sm-12 col-md-12 col-lg-5 link-article-categorie">
+                            <!-- Category title -->
+                            <h5 class="category-technologies">Technologie</h5>
+                            <!-- Link single article connexe -->
+                            <a href="#" id="">Renforcement du créneau de l'électrification des transports à Shawinigan</a>
+                            <!-- Author article connexe -->
+                            <p class="author-article-connexe"><span class="date-article-connexe">28 mars 2017</span> / par Denis Morin</p>
+                        </div>
+
+                        <!-- SINGLE block article connexe -->
+                        <div class="col-xs-10 col-sm-12 col-md-12 col-lg-5 col-lg-offset-1 link-article-categorie">
+                            <!-- Category title -->
+                            <h5 class="category-technologies">Technologie</h5>
+                            <!-- Link single article connexe -->
+                            <a href="#" id="">Renforcement du créneau de l'électrification des transports à Shawinigan</a>
+                            <!-- Author article connexe -->
+                            <p class="author-article-connexe"><span class="date-article-connexe">28 mars 2017</span> / par Denis Morin</p>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding article-categorie">
+                        <!-- SINGLE block article connexe -->
+                        <div class="col-xs-10 col-sm-12 col-md-12 col-lg-5 link-article-categorie">
+                            <!-- Category title -->
+                            <h5 class="category-technologies">Technologie</h5>
+                            <!-- Link single article connexe -->
+                            <a href="#" id="">Renforcement du créneau de l'électrification des transports à Shawinigan</a>
+                            <!-- Author article connexe -->
+                            <p class="author-article-connexe"><span class="date-article-connexe">28 mars 2017</span> / par Denis Morin</p>
+                        </div>
+
+                        <!-- SINGLE block article connexe -->
+                        <div class="col-xs-10 col-sm-12 col-md-12 col-lg-5 col-lg-offset-1 link-article-categorie">
+                            <!-- Category title -->
+                            <h5 class="category-technologies">Technologie</h5>
+                            <!-- Link single article connexe -->
+                            <a href="#" id="">Renforcement du créneau de l'électrification des transports à Shawinigan</a>
+                            <!-- Author article connexe -->
+                            <p class="author-article-connexe"><span class="date-article-connexe">28 mars 2017</span> / par Denis Morin</p>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding article-categorie">
+                        <!-- SINGLE block article connexe -->
+                        <div class="col-xs-10 col-sm-12 col-md-12 col-lg-5 link-article-categorie">
+                            <!-- Category title -->
+                            <h5 class="category-technologies">Technologie</h5>
+                            <!-- Link single article connexe -->
+                            <a href="#" id="">Renforcement du créneau de l'électrification des transports à Shawinigan</a>
+                            <!-- Author article connexe -->
+                            <p class="author-article-connexe"><span class="date-article-connexe">28 mars 2017</span> / par Denis Morin</p>
+                        </div>
+
+                        <!-- SINGLE block article connexe -->
+                        <div class="col-xs-10 col-sm-12 col-md-12 col-lg-5 col-lg-offset-1 link-article-categorie">
+                            <!-- Category title -->
+                            <h5 class="category-technologies">Technologie</h5>
+                            <!-- Link single article connexe -->
+                            <a href="#" id="">Renforcement du créneau de l'électrification des transports à Shawinigan</a>
+                            <!-- Author article connexe -->
+                            <p class="author-article-connexe"><span class="date-article-connexe">28 mars 2017</span> / par Denis Morin</p>
+                        </div>
+                    </div>
+
+                </div><!-- /.row -->
+            </section>
+		</div><!-- /.container -->
+       
+        <!-- Arrows Section Catégories -->
+		<div class="container-fluid no-padding no-margin">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 top-bar-section arrows-bar">
+                <i class="col-xs-6 col-sm-6 col-md-6 col-lg-3 col-lg-offset-3 fa fa-arrow-circle-o-left" aria-hidden="true"></i>
+                <i class="col-xs-6 col-sm-6 col-md-6 col-lg-3 fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+            </div>
+		</div>          
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="/js/blogue.js"></script>
+	</body>
+</html>
