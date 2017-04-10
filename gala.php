af4f412fe50fa4599021878bcd8019e45b1b47de
Dave <dave_bussiere@hotmail.com>
2017-04-09 22:29:34 -0400

    DONE // page Gala
    DONE // page Gala
    
    Voir fichiers : gala.php + gala.css
    
    - intégration statique, OK
    - fullscreen, OK
    - responsive, OK

---
+++ gala.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>GALA DISTINCTION DESJARDINS</title>
+		<script src="https://use.fontawesome.com/663ece6c6a.js"></script>
+		
+		<!-- GOOGLE FONT -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/gala.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+        <!-- Top bar Section Article -->
+		<div class="container-fluid no-padding no-margin">
+            
+            <!-- Top Bar -->
+			<h2 class="top-bar-section gala">Le Gala Distinction Desjardins</h2>
+
+            <!-- Title Gala Distinction -->
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-lg-offset-2 title-gala">
+                <h3>Le Gala Distinction Desjardins, c'est votre soirée!</h3>
+            </div>
+
+		</div>
+
+        <!-- Gradient Background -->
+        <div class="gradient-background">
+
+            <!-- Contenu Section Gala -->
+            <div class="container no-padding">
+
+                <!-- Section Gala -->
+                <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 section-gala">
+                    
+                    <!-- BLOC ÉDITION -->
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 bloc-gala-edition">
+
+                            <!-- image tag -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 edition-tag no-padding no-margin">
+                                <img src="img/img-tag-edition.png" alt="" class="img-responsive">
+                                <h3>Édition 2017</h3>
+                            </div>
+
+                        <div class="row">
+
+                            <!--  Subtitle + description -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 edition-year-title">
+                                <h2>Le Gala Distinction Desjardins 2017</h2>
+                                <p>Découvrez nos <a href="">4 finalistes</a> pour le nom du prix Personnalité d'affaires féminine. Le nom choisi sera dévoilé en juin 2017.</p>
+                            </div>
+
+                        </div><!-- /.row -->
+
+                        <div class="row">
+
+                            <!--  Navigation Finalistes Féminines -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+                                <!-- Navigation -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 nav-finaliste-feminine">
+
+                                    <ul class="no-padding">
+                                        <li class="col-xs-12 col-sm-12 col-md-12 col-lg-6"><a href="">Jeannette Laflamme</a></li>
+                                        <li class="col-xs-12 col-sm-12 col-md-12 col-lg-6"><a href="">Pauline B. Allard</a></li>
+                                        <li class="col-xs-12 col-sm-12 col-md-12 col-lg-6"><a href="">Lise Lavergne</a></li>
+                                        <li class="col-xs-12 col-sm-12 col-md-12 col-lg-6"><a href="">Jeanne Fortin</a></li>
+                                    </ul>
+
+                                </div>
+
+                                <!--  Text -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-finaliste">
+                                    <p>Un merci tout spécial aux familles de nos finalistes et à Madeleine Lacoursière de la Société d'histoire et de généalogie de Shawinigan qui ont contribué à la recherche et à la rédaction des biographies.</p>
+                                    <p>Vous pouvez faire connaître votre choix par courriel à Martin St-Pierre, directeur général de la CCIS:</p>
+                                    <a href="#">martin@ccishawinigan.ca</a>
+                                </div>
+
+                            </div><!-- /.Navigation Finalistes Féminines -->
+
+                            <!-- Historique -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bloc-historique">
+                                <h3>Historique</h3>
+                                <p>En collaboration avec d’importants partenaires économiques, le Gala Distinction Desjardins honore et reconnaît depuis plus de 20 ans les efforts des entrepreneurs de la région. Jusqu’à maintenant, pas moins de 250 entreprises se sont fait attribuer le titre de meilleure entreprise dans sa catégorie, ce qui représente près de 750 distinctions soulignées dans le milieu des affaires.  Chaque année, le Gala rassemble au-delà de 400 personnes impliquées de près ou de loin dans milieu des affaires. Pour beaucoup, il s’agit d’un accès privilégié à cette communauté et d’un rayonnement incomparable pour l’entreprise.</p>
+                            </div>
+
+                        </div><!-- /.row -->
+                        
+                    </div><!-- /.bloc-gala-edition -->
+
+                    <!-- BLOC FINALISTE -->
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 bloc-gala-finaliste">
+
+                            <!-- image tag -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 edition-tag no-padding no-margin">
+                                <img src="img/img-tag-finaliste.png" alt="" class="img-responsive">
+                                <h3>Être finaliste</h3>
+                            </div>
+
+                        <div class="row">
+
+                            <!--  Subtitle + description -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 etre-finaliste">
+                                <h2>Les avantages d’être finaliste !</h2>
+                                <p>Être finaliste au Gala Distinction Desjardins est une superbe carte de visite. Il s’agit d’une  reconnaissance concrète des intervenants économiques de la région du succès de l'entreprise. Mais c'est aussi un bel ajout au CV corporatif.</p>
+                                <p>Depuis plus de 20 ans, la Chambre de commerce décerne ces prix aux entrepreneurs locaux. Ils sont donc nombreux à afficher fièrement le trophée reçu en reconnaissance de leurs efforts. Les succès de ces entrepreneurs sont reconnus chaque année devant plus de 400 personnalités d'affaires. Sans compter les clients et partenaires qui peuvent voir cette récompense lors de leur passage dans l'entreprise. Deux bonnes raisons de se dépasser chaque année et de présenter à nouveau, ou pour la première fois, sa candidature au Gala Distinction Desjardins.</p>
+                                <p>Jusqu’à maintenant, le Gala a permis à près d’une centaine d’entreprises de se faire connaître par un vaste public. Cela représente une occasion incroyable de réseauter et de se faire reconnaître par le milieu des affaires.</p>
+                                <p>Outre la visibilité offerte à même la soirée du Gala, vous profiterez d’une visibilité incomparable auprès des médias et du réseau de la Chambre. Ce placement médiatique équivaut à quelques milliers de dollars de publicité auprès de votre public cible.</p>
+                                <p>De plus, être finaliste apporte une grande crédibilité à l'entreprise. Cette visibilité médiatique est donc appelée à perdurer, puisque les médias se tourneront vers des entreprises crédibles pour leurs différents dossiers par exemple. Les clients et partenaires seront aussi séduits par cette valeur ajoutée.</p>
+                                <p>Être finaliste au Gala Distinction Desjardins apporte tant de retombées positives! Pourquoi attendre avant de déposer votre candidature pour la prochaine édition?</p>
+                            </div>
+
+                        </div><!-- /.row -->
+
+                        <div class="row">
+
+                            <!-- Bouton Devenir Finaliste -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+                                <!-- Bouton Finaliste -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-lg-offset-2 btn-devenir-finaliste">
+                                    <a href="" class="btn-finaliste"><i class="fa fa-trophy" aria-hidden="true"></i>Devenez finaliste</a>
+                                </div>
+                                
+                            </div><!-- /.Button Finaliste -->
+
+                        </div><!-- /.row -->
+
+                    </div><!-- /.bloc-gala-finaliste -->
+                </section><!-- /.Section Gala -->
+
+            </div><!-- /.container -->
+        </div><!-- /.gradient-background -->
+
+        <div class="container-fluid editions-precedentes">
+            
+            <div class="row">
+
+                <!-- Title + Subtitle -->
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <h2>Gagnants des éditions précédentes</h2>
+                    <p>Vous pouvez naviguer ci-dessous les gagnants des éditions précédentes ainsi que la liste des différentes catégories.</p>
+                </div>
+
+                <!-- Nav Éditions précédentes -->
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-lg-offset-3">
+                    <ul class="no-padding">
+                        <li class="col-xs-12 col-sm-12 col-md-12 col-lg-6"><a href="">Catégories 2016</a></li>
+                        <li class="col-xs-12 col-sm-12 col-md-12 col-lg-6"><a href="">Lauréats 2016</a></li>
+                    </ul>
+                </div>
+
+            </div><!-- /.row -->
+        </div><!-- /.editions-precedentes -->
+
+
+        <!-- Inner Top Shadow -->
+        <div class="inner-top-shadow">
+
+            <!-- Contenu Section Rabais + Gala Devenez Finaliste -->
+            <div class="container no-padding">
+
+                <div class="row">
+
+                    <!-- Description Section -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin section-promo-description">
+                        <h2>Une bonne relation d'affaires ça commence ici!</h2>
+                        <p>La Chambre met en valeur le succès économique régional des entreprises et des gens d’affaires de la région. Elle intervient de façon vigilante et constructive auprès des instances et autorités afin de défendre ses membres. Elle stimule le réseautage de ses membres et développe les compétences des gens d’affaires en offrant de l’information, de l’expertise es et en les aiguillant vers les ressources locales.</p>
+                    </div>
+                    
+
+
+                    <!-- Bloc Promo et Pub -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+
+                        <!-- Section Pub Gala -->
+                        <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 section-pub-gala">
+
+                            <!-- Gala Distinction -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-lg-push-6 no-padding no-margin">
+
+                                <h2>Gala Distinction Desjardins<span class="gala-subtitle-normal">Démarquez-vous et soyez reconnu!</span></h2>
+                                <p>Chaque année, le Gala rassemble au-delà de 400 personnes impliquées de près ou de loin dans milieu des affaires. Pour beaucoup, il s’agit d’un accès privilégié à cette communauté et d’un rayonnement incomparable pour l’entreprise.</p>
+                                <p class="les-avantages">Découvrez les avantages d’être finaliste :</p>
+
+                                <!-- Bouton inscription -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 btn-devenir-finaliste">
+                                    <a href="" class="btn-finaliste"><i class="fa fa-trophy" aria-hidden="true"></i>Devenez finaliste</a>
+                                    <a href="" class="savoir-plus">En savoir plus</a>
+                                </div>
+
+                            </div>
+
+                        </section><!-- /.Section Pub Gala -->
+
+                        <!-- Section Rabais -->
+                        <section class="col-xs-12 col-sm-12 col-md-6 col-lg-6 section-promo-rabais">
+
+                            <!-- Promo 25% -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+                                <h2>Être membre c’est<span class="rabais-bold">25% de rabais</span><span class="rabais-subtitle">sur les tous les évènements</span></h2>
+                            </div>
+
+                            <!-- Double Adhésion -->
+                            <aside class="col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-12 col-lg-8 col-lg-offset-2 double-adhesion">
+
+                                <!-- Title -->
+                                <h2>Double-adhésion!</h2>
+
+                                <!-- Text -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    <p>Obtenez le double de services avec la double-adhésion CCIS + Jeune Chambre de commerce de la Mauricie</p>
+                                    <a href="">jccm.qc.ca</a>
+                                </div>
+
+                            </aside>
+
+                            <!-- Devenir Inscription -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+
+                                <!-- Bouton inscription -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 inscription-membre-ccis">
+                                    <a href="" class="btn-inscription"><i class="fa fa-user" aria-hidden="true"></i><span>Oui</span>, je m'inscris maintenant</a>
+                                    <a href="" class="savoir-plus">En savoir plus</a>
+                                </div>
+                                
+                            </div><!-- /.Button Contact -->
+
+                        </section><!-- /.Section Promo Rabais -->                
+
+                        <!-- Logo circle -->
+                        <div class="position-logo-ccis">
+                            
+                            <!-- image logo -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 logo-circle">
+                                <img src="img/img-circle-logo-ccis.png" alt="" class="img-responsive img-logo-ccis">
+                            </div>
+
+                        </div>
+
+                    </div><!-- /.Bloc Promo et Pub -->
+                </div><!-- /.row -->
+                
+            </div><!-- /.container -->
+        </div><!-- inner-top-shadow -->
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="/js/blogue.js"></script>
+	</body>
+</html>
