d57024346f7d8f0ec72d8639be17d18b34b603d0
Dave <dave_bussiere@hotmail.com>
2017-04-10 23:30:38 -0400

    IN-PROGESS // page La Chambre
    IN-PROGESS // page La Chambre
    
    Fait au 2/3
    
    - intégration statique, OK
    - fullscreen, OK
    - responsive, OK

---
+++ chambre.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>LA CHAMBRE</title>
+		<script src="https://use.fontawesome.com/663ece6c6a.js"></script>
+		
+		<!-- GOOGLE FONT -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/chambre.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+        <!-- Top bar Implications -->
+		<div class="container-fluid no-padding">
+
+            <!-- Top Bar -->
+			<h2 class="top-bar-section chambre">La chambre</h2>
+
+            <!-- Sous-titre + description -->
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 top-bar-section-content">
+                <h3 class="hidden-mobile">La CCIS est un réseau structuré de plus de 600 gens d'affaires <span>représentant la communauté d'affaires de Shawinigan</span></h3>
+                <h3 class="visible-mobile">La CCIS est un réseau structuré de plus de<span>600 gens d'affaires</span></h3>
+                <p>La Chambre de commerce et d’industrie de Shawinigan est un acteur incontournable dans le développement économique de Shawinigan et de la Mauricie.</p>
+            </div>
+
+		</div>
+
+        <!-- Bloc CCIS en 10 POINTS -->
+        <div class="pattern-background">
+            <div class="container">
+
+                <div class="row">
+
+                    <!-- Content -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bloc-ccis-10-pts">
+
+                        <!-- Title -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1">
+                            <h2>La Chambre de commerce <span>et d'industrie de Shawinigan</span></h2>
+                        </div>
+
+                        <!-- Sub-title -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <h3>La CCIS en 10 points c’est :</h3>
+                        </div>           
+                        
+                        <!-- Bullet list text -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1">
+
+                            <!-- Bullet list -->
+                            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 ccis-bullet-list">
+                                <ul>
+                                    <li>La voix de la communauté d'affaires de la région.</li>
+                                    <li>Est reconnue comme le plus important mouvement d'affaires de la région.</li>
+                                    <li>Possède de l'expertise au service des gens d'affaires.</li>
+                                    <li>Rassemble tous les secteurs d'activités économiques	et socio-économiques de la région.</li>
+                                    <li>Favorise la promotion de l'activité commerciale.</li>
+                                </ul>
+                            </div><!-- /.bullet list -->
+
+                            <!-- Bullet list -->
+                            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 ccis-bullet-list">
+                                <ul>
+                                    <li>Crée de multiples occasions de réseautage d’affaires.</li>
+                                    <li>Stimule et dynamise le milieu économique par ses initiatives.</li>
+                                    <li>Un conseil d'administration dévoué et une équipe dynamique à la permanence.</li>
+                                    <li>Une mission économique et des valeurs organisationnelles.</li>
+                                    <li>Une implication sociale au sein de nombreux comités.</li>
+                                </ul>
+                            </div>
+
+                        </div><!-- /.bullet list -->
+
+                    </div><!-- /.content -->
+                </div><!-- /.row -->
+            </div><!-- /.container-->
+        </div><!-- /.Bloc CCIS en 10 POINTS -->
+
+        <div class="inner-shadow"></div>
+
+        <!-- Mot dirigeant -->
+        <div class="gray-background">
+            <div class="container">
+
+                <div class="row">
+
+                    <!-- Bloc image dirigeant -->
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding no-margin bloc-img-dirigeant">
+                        
+                        <!-- Image -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <img src="img/img-default-dirigeant.png" alt="" class="img-responsive img-dirigeant">
+                            <h3>M. Mario Lamontagne<span>président de la CCIS</span></h3>
+                        </div>
+
+                    </div><!-- /.image-dirigeant -->
+
+                    <!-- Bloc image dirigeant -->
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding no-margin mot-du-dirigeant">
+                        
+                        <!-- Image -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <h2>Mot du président</h2>
+                            <p>In gravida blandit sem. Morbi imperdiet commodo libero id tincidunt. Nunc suscipit malesuada diam, a feugiat tortor tempor in. Nulla posuere sed orci in sodales. Nam at dolor nibh. Curabitur id risus id tellus pellentesque imperdiet. Quisque ac justo tristique, varius purus quis, aliquet felis. Sed eget blandit nisi, et ultrices massa. Nunc lectus nibh, porttitor lacinia mi rutrum, condimentum eleifend ex.</p>
+                            <p>Nam auctor fermentum neque, sit amet condimentum risus ultrices et. Curabitur eu ornare magna. Vestibulum consequat sapien diam, in auctor nunc efficitur eget.</p>
+                            <p>Nullam gravida non sem et consequat. Fusce imperdiet libero non urna scelerisque fermentum. Quisque orci lorem, blandit vitae dui nec, mollis maximus metus.</p>
+                            <p>Vivamus ultrices metus at ligula egestas, non bibendum libero molestie. Nam at est nec massa cursus lacinia non varius eros. Etiam condimentum molestie scelerisque.</p>
+                        </div>
+
+                        <!-- Bouton inscription -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 email-contact">
+                            <a href="" class="btn-email-contact">mlamontagne@deloitte.ca</a>
+                        </div>
+
+                    </div><!-- /.mot-du-dirigeant -->
+
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </div><!-- /.gray-background -->
+
+        <!-- Mot dirigeant -->
+        <div class="gray-background">
+            <div class="container">
+
+                <div class="row">
+
+                    <!-- Bloc image dirigeant -->
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding no-margin bloc-img-dirigeant">
+                        
+                        <!-- Image -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <img src="img/img-default-dirigeant.png" alt="" class="img-responsive img-dirigeant">
+                            <h3>M. Martin St-Pierre<span>directeur général de la CCIS</span></h3>
+                        </div>
+
+                    </div><!-- /.image-dirigeant -->
+
+                    <!-- Bloc image dirigeant -->
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 no-padding no-margin mot-du-dirigeant">
+                        
+                        <!-- Image -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <h2>Mot du directeur général</h2>
+                            <p>Nam auctor fermentum neque, sit amet condimentum risus ultrices et. Curabitur eu ornare magna. Vestibulum consequat sapien diam, in auctor nunc efficitur eget. Nullam gravida non sem et consequat. Fusce imperdiet libero non urna scelerisque fermentum. Quisque orci lorem, blandit vitae dui nec, mollis maximus metus.</p>
+                            <p>Vivamus ultrices metus at ligula egestas, non bibendum libero molestie. Nam at est nec massa cursus lacinia non varius eros. Etiam condimentum molestie scelerisque. Duis nunc eros, venenatis vitae dictum vel, ullamcorper sit amet dui. Nam rutrum leo eget viverra accumsan. Ut scelerisque dignissim gravida. Nullam sed bibendum dolor.</p>
+                            <p>In gravida blandit sem. Morbi imperdiet commodo libero id tincidunt. Nunc suscipit malesuada diam, a feugiat tortor tempor in. Nulla posuere sed orci in sodales. Nam at dolor nibh. Curabitur id risus id tellus pellentesque imperdiet. Quisque ac justo tristique, varius purus quis, aliquet felis. Sed eget blandit nisi, et ultrices massa. Nunc lectus nibh, porttitor lacinia mi rutrum, condimentum eleifend ex.</p>
+                        </div>
+
+                        <!-- Bouton inscription -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 email-contact">
+                            <a href="" class="btn-email-contact">martin@ccishawinigan.ca</a>
+                        </div>
+
+                    </div><!-- /.mot-du-dirigeant -->
+
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </div><!-- /.gray-background -->
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="/js/blogue.js"></script>
+	</body>
+</html>
