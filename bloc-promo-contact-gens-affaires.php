d7ecb4cc6057c91a88e2be87008be30c5b5a1aac
Dave <dave_bussiere@hotmail.com>
2017-04-12 07:41:48 -0400

    BLOCS PROMO isolés // voir détails
    BLOCS PROMO isolé // voir détails
    
    J’ai isolé dans des fichiers séparés (php + css) les blocs
    promotionnels :
    
    - “Rabais 25% + Gala”
    - “Contactez-nous + 600 Gens d’affaires”

---
+++ bloc-promo-contact-gens-affaires.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>NOS PARTENAIRES</title>
+		<script src="https://use.fontawesome.com/663ece6c6a.js"></script>
+		
+		<!-- GOOGLE FONT -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/bloc-promo-contact-gens-affaires.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+
+        <!-- Inner Top Shadow -->
+        <div class="inner-top-shadow">
+
+            <!-- Contenu Contact + 600 gens d'affaires' -->
+            <div class="container no-padding">
+
+                <div class="row">
+
+                    <!-- Description Section -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin section-promo-description">
+                        <h2>Une bonne relation d'affaires ça commence ici!</h2>
+                        <p>La Chambre met en valeur le succès économique régional des entreprises et des gens d’affaires de la région. Elle intervient de façon vigilante et constructive auprès des instances et autorités afin de défendre ses membres. Elle stimule le réseautage de ses membres et développe les compétences des gens d’affaires en offrant de l’information, de l’expertise es et en les aiguillant vers les ressources locales.</p>
+                    </div>
+
+                    <!-- Bloc Promo et Pub -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+
+                        <!-- Section Gens d'affaires -->
+                        <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 section-gens-affaires">
+
+                            <!-- Gens d'affaires -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-lg-push-6 no-padding no-margin">
+
+                                <!-- Bouton devenir membre -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 btn-devenir-membre">
+                                    <a href="" class="btn-membre"><i class="fa fa-user" aria-hidden="true"></i>Devenir membre</a>
+                                </div>
+
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1">
+                                    <h2><span>Un réseau de plus de</span>600 gens d'affaires</h2>
+                                </div>
+
+                                <!-- Bouton inscription évènement -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 btn-inscription-evenement">
+                                    <a href="" class="btn-inscription"><i class="fa fa-users" aria-hidden="true"></i>Inscription évènement</a>
+                                </div>
+
+                            </div>
+
+                        </section><!-- /.Section Gens d'affaires -->
+
+                        <!-- Section Contact -->
+                        <section class="col-xs-12 col-sm-12 col-md-6 col-lg-6 section-contactez-nous">
+
+                            <!-- Contact -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+
+                                <!-- Bouton Contact -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 bloc-contactez-nous">
+                                    <a href="" class="btn-contactez-nous"><i class="fa fa-comments" aria-hidden="true"></i>Contactez-nous</a>
+                                </div>
+
+                            </div><!-- /.Button Contact -->
+
+                            <!-- Contact infos -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 info-contact no-padding no-margin">
+                                <h2><span class="telephone-bold">819 536-0777</span><a href="" class="contact-email">info@ccishawinigan.ca</a></h2>
+                                <p>1250, avenue de la Station, bureau 1-116<span>Shawinigan (Québec) G9N 1W8</span></p>
+                            </div>
+
+                        </section><!-- /.Section Promo Rabais -->
+
+                        <!-- Logo circle -->
+                        <div class="position-logo-ccis">
+
+                            <!-- image logo -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 logo-circle">
+                                <img src="img/img-circle-logo-ccis.png" alt="" class="img-responsive img-logo-ccis">
+                            </div>
+
+                        </div>
+
+                    </div><!-- /.Bloc Promo et Pub -->
+                </div><!-- /.row -->
+                
+            </div><!-- /.container -->
+        </div><!-- inner-top-shadow -->
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="/js/blogue.js"></script>
+	</body>
+</html>
---
+++ bloc-promo-rabais-distinction.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>GALA DISTINCTION DESJARDINS</title>
+		<script src="https://use.fontawesome.com/663ece6c6a.js"></script>
+		
+		<!-- GOOGLE FONT -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/gala.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+
+        <!-- Inner Top Shadow -->
+        <div class="inner-top-shadow">
+
+            <!-- Contenu Section Contact + Gens d'affaires -->
+            <div class="container no-padding">
+
+                <div class="row">
+
+                    <!-- Description Section -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin section-promo-description">
+                        <h2>Une bonne relation d'affaires ça commence ici!</h2>
+                        <p>La Chambre met en valeur le succès économique régional des entreprises et des gens d’affaires de la région. Elle intervient de façon vigilante et constructive auprès des instances et autorités afin de défendre ses membres. Elle stimule le réseautage de ses membres et développe les compétences des gens d’affaires en offrant de l’information, de l’expertise es et en les aiguillant vers les ressources locales.</p>
+                    </div>
+
+                    <!-- Bloc Promo et Pub -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+
+                        <!-- Section Pub Gala -->
+                        <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 section-pub-gala">
+
+                            <!-- Gala Distinction -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-lg-push-6 no-padding no-margin">
+
+                                <h2>Gala Distinction Desjardins<span class="gala-subtitle-normal">Démarquez-vous et soyez reconnu!</span></h2>
+                                <p>Chaque année, le Gala rassemble au-delà de 400 personnes impliquées de près ou de loin dans milieu des affaires. Pour beaucoup, il s’agit d’un accès privilégié à cette communauté et d’un rayonnement incomparable pour l’entreprise.</p>
+                                <p class="les-avantages">Découvrez les avantages d’être finaliste :</p>
+
+                                <!-- Bouton inscription -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 btn-devenir-finaliste">
+                                    <a href="" class="btn-finaliste"><i class="fa fa-trophy" aria-hidden="true"></i>Devenez finaliste</a>
+                                    <a href="" class="savoir-plus">En savoir plus</a>
+                                </div>
+
+                            </div>
+
+                        </section><!-- /.Section Pub Gala -->
+
+                        <!-- Section Rabais -->
+                        <section class="col-xs-12 col-sm-12 col-md-6 col-lg-6 section-promo-rabais">
+
+                            <!-- Promo 25% -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+                                <h2>Être membre c’est<span class="rabais-bold">25% de rabais</span><span class="rabais-subtitle">sur les tous les évènements</span></h2>
+                            </div>
+
+                            <!-- Double Adhésion -->
+                            <aside class="col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-12 col-lg-8 col-lg-offset-2 double-adhesion">
+
+                                <!-- Title -->
+                                <h2>Double-adhésion!</h2>
+
+                                <!-- Text -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    <p>Obtenez le double de services avec la double-adhésion CCIS + Jeune Chambre de commerce de la Mauricie</p>
+                                    <a href="">jccm.qc.ca</a>
+                                </div>
+
+                            </aside>
+
+                            <!-- Devenir Inscription -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+
+                                <!-- Bouton inscription -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 inscription-membre-ccis">
+                                    <a href="" class="btn-inscription"><i class="fa fa-user" aria-hidden="true"></i><span>Oui</span>, je m'inscris maintenant</a>
+                                    <a href="" class="savoir-plus">En savoir plus</a>
+                                </div>
+                                
+                            </div><!-- /.Button Contact -->
+
+                        </section><!-- /.Section Promo Rabais -->                
+
+                        <!-- Logo circle -->
+                        <div class="position-logo-ccis">
+                            
+                            <!-- image logo -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 logo-circle">
+                                <img src="img/img-circle-logo-ccis.png" alt="" class="img-responsive img-logo-ccis">
+                            </div>
+
+                        </div>
+
+                    </div><!-- /.Bloc Promo et Pub -->
+                </div><!-- /.row -->
+                
+            </div><!-- /.container -->
+        </div><!-- inner-top-shadow -->
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="/js/blogue.js"></script>
+	</body>
+</html>
