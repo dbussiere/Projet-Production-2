a0b3f1c833f9bce5fe53abceab572fcce4412e82
Dave <dave_bussiere@hotmail.com>
2017-04-11 21:15:36 -0400

    DONE // page Répertoire des membres
    DONE // page Répertoire des membres
    
    Voir fichiers : repertoire-membre.php + repertoire-membre.css
    
    - intégration statique, OK
    - fullscreen, OK
    - responsive, OK

---
+++ repertoire-membre.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>RÉPERTOIRE DES MEMBRES</title>
+		<script src="https://use.fontawesome.com/663ece6c6a.js"></script>
+		
+		<!-- GOOGLE FONT -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/repertoire-membre.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+        <!-- Top bar Implications -->
+		<div class="container-fluid no-padding">
+
+            <!-- Top Bar -->
+			<h2 class="top-bar-section chambre">Répertoire des membres</h2>
+
+            <!-- Sous-titre + description -->
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 top-bar-section-content">
+                <h3>Pour un réseautage actif!</h3>
+                <p>La CCIS organise plus de 10 activités par année afin de favoriser le réseautage d’affaires entre les gens d’affaires de la région. De plus, nos conférences, lunchs d'affaires ciblés et autres activités peuvent vous permettre d'entrer en contact avec des gens et des entreprises en lien direct avec votre domaine d'affaires.</p>
+            </div>
+
+		</div>
+
+        <!-- Recherche Répertoire DESKTOP et autres format -->
+        <div class="container">
+
+            <div class="row">
+
+                <!-- Section Recheche -->
+                <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+                    <!-- LEFT Part - Fiche Membre -->
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 fiche-membre">
+                        
+                        <!-- Image logo membre -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bloc-img-logo-membre">
+                            <img src="img/img-logo-membre-smartic.png" alt="" class="img-responsive img-logo-membre">
+                        </div>
+                            
+                        <!-- Bloc Nom Membre -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 nom-membre no-padding no-margin">
+
+                            <!-- Nom Membre -->
+                            <h2>Smartic</h2>
+
+                        </div><!-- /.Bloc Nom Membre -->
+                            
+                        <!-- Bloc Infos Membre -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 infos-membre no-padding no-margin">
+                            
+                            <!-- Infos Membre -->
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nisl lacus, hendrerit quis pharetra ac, tempus eu orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec non auctor tellus. Proin iaculis magna fermentum nulla suscipit efficitur.</p>
+                            <p>Mauris ut mauris non diam rutrum fermentum id eu felis. Quisque ut mi placerat, consectetur eros nec, blandit metus. Quisque in enim eget nisi condimentum porttitor. Pellentesque accumsan accumsan quam, sed facilisis purus facilisis quis. Praesent non commodo orci.</p>
+
+                        </div><!-- /.Bloc Infos Membre -->
+
+                        <!-- Bloc Secteur Activités Membre -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 infos-membre no-padding no-margin">
+                            
+                            <!-- Secteur activités Membre -->
+                            <h3>Secteur(s) d'activité(s) :</h3>
+                            
+                            <!-- Bullet list -->
+                            <ul>
+                                <li>Web</li>
+                                <li>Médias & communication</li>
+                                <li>Marketing, publicité</li>
+                            </ul>
+
+                        </div><!-- /.Bloc Secteur Activités Membre -->
+
+                        <!-- Bloc Contact Membre -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 infos-membre no-padding no-margin">
+                            
+                            <!-- Secteur activités Membre -->
+                            <h3>Courriel :<a href="">bonjour@smartic.ca</a></h3>
+
+                        </div><!-- /.Bloc Secteur Activités Membre -->
+
+                    </div><!-- /. LEFT PART -->
+
+                    <!-- RIGHT Part - Champ recherche -->
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 bloc-recherche">
+                        
+                        <!-- Bloc Catégorie -->
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bloc-categorie styled-select no-padding no-margin">
+                            
+                            <!-- Choix catégorie -->
+                            <select name="" id="" class="select-default" required="">
+                                <option value="" selected>Sélectionnez une catégorie</option>
+                                <option value="">Agence de voyage</option>
+                                <option value="">Alimentation, emboutaillage, distributeur</option>
+                                <option value="">Animalerie</option>
+                                <option value="">Architecte</option>
+                                <option value="">Arpenteur</option>
+                                <option value="">Arts, culture, loisirs</option>
+                                <option value="">Associations, gouvernements, organismes</option>
+                                <option value="">Assureur</option>
+                                <option value="">Automobile</option>
+                                <option value="">Avocat</option>
+                                <option value="">Bistro, bar</option>
+                                <option value="">Bureautique, informatique, photocopie</option>
+                                <option value="">Cabane à sucre</option>
+                                <option value="">Centre commercial</option>
+                                <option value="">Commerce</option>
+                                <option value="">Communications</option>
+                                <option value="">Comptable</option>
+                                <option value="">Construction</option>
+                                <option value="">Courtier</option>
+                                <option value="">Designer</option>
+                                <option value="">Économie sociale et communautaire</option>
+                                <option value="">Éducation</option>
+                                <option value="">Énergie</option>
+                                <option value="">Évaluateur</option>
+                                <option value="">Ferme</option>
+                                <option value="">Fiscaliste</option>
+                                <option value="">Fleuriste</option>
+                                <option value="">Garderie</option>
+                                <option value="">Hôpital vétérinaire</option>
+                                <option value="">Immobilier</option>
+                                <option value="">Industriel, manufacturier</option>
+                                <option value="">Ingénieur</option>
+                                <option value="">Institutions financières</option>
+                                <option value="">Jeune entrepreneur, professionnel</option>
+                                <option value="">Marketing, publicité</option>
+                                <option value="">Médias et communication</option>
+                                <option value="">Notaire</option>
+                                <option value="">Produits pharmaceutiques</option>
+                                <option value="">Professionnels de la santé</option>
+                                <option value="">Résidence pour personnes âgées</option>
+                                <option value="">Ressources humaines</option>
+                                <option value="">Restaurant</option>
+                                <option value="">Salon funéraire</option>
+                                <option value="">Santé et beauté</option>
+                                <option value="">Sécurité, loi, justice</option>
+                                <option value="">Services financiers</option>
+                                <option value="">Services techniques</option>
+                                <option value="">Syndic</option>
+                                <option value="">Télécommunications</option>
+                                <option value="">Tourisme et hébergement</option>
+                                <option value="">Traiteur</option>
+                                <option value="">Transports</option>
+                                <option value="">Travailleur autonome</option>
+                                <option value="">Web</option>
+                            </select>
+
+                            <!-- Titre H6 positionné relative/absolute par-dessus le select custom css -->
+                            <h6 class="select-default-text">Sélectionnez une catégorie</h6>
+
+                        </div><!-- /.Bloc Catégorie -->
+                        
+                        <div class="row">
+                        
+                            <!-- Résultats de recherche -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 search-result">
+
+                                <!-- Bullet list des résultats -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    <ul>
+                                        <li><a href="">Smartic</a></li>
+                                        <li><a href="">Absolu</a></li>
+                                        <li><a href="">ADN</a></li>
+                                    </ul>
+                                </div>
+
+                            </div><!-- /. résultat de recherche -->
+                        
+                        </div><!-- /.row -->
+
+                    </div><!-- /. RIGHT PART -->
+                </section><!-- /.Section Recheche -->
+
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+
+        <!-- Inner Top Shadow -->
+        <div class="inner-top-shadow">
+
+            <!-- Contenu Section Rabais + Gala Devenez Finaliste -->
+            <div class="container no-padding">
+
+                <div class="row">
+
+                    <!-- Description Section -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin section-promo-description">
+                        <h2>Une bonne relation d'affaires ça commence ici!</h2>
+                        <p>La Chambre met en valeur le succès économique régional des entreprises et des gens d’affaires de la région. Elle intervient de façon vigilante et constructive auprès des instances et autorités afin de défendre ses membres. Elle stimule le réseautage de ses membres et développe les compétences des gens d’affaires en offrant de l’information, de l’expertise es et en les aiguillant vers les ressources locales.</p>
+                    </div>
+
+                    <!-- Bloc Promo et Pub -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+
+                        <!-- Section Gens d'affaires -->
+                        <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 section-gens-affaires">
+
+                            <!-- Gens d'affaires -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-lg-push-6 no-padding no-margin">
+
+                                <!-- Bouton devenir membre -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 btn-devenir-membre">
+                                    <a href="" class="btn-membre"><i class="fa fa-user" aria-hidden="true"></i>Devenir membre</a>
+                                </div>
+
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1">
+                                    <h2><span>Un réseau de plus de</span>600 gens d'affaires</h2>
+                                </div>
+
+                                <!-- Bouton inscription évènement -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 btn-inscription-evenement">
+                                    <a href="" class="btn-inscription"><i class="fa fa-users" aria-hidden="true"></i>Inscription évènement</a>
+                                </div>
+
+                            </div>
+
+                        </section><!-- /.Section Gens d'affaires -->
+
+                        <!-- Section Contact -->
+                        <section class="col-xs-12 col-sm-12 col-md-6 col-lg-6 section-contactez-nous">
+
+                            <!-- Contact -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+
+                                <!-- Bouton Contact -->
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-lg-offset-1 bloc-contactez-nous">
+                                    <a href="" class="btn-contactez-nous"><i class="fa fa-comments" aria-hidden="true"></i>Contactez-nous</a>
+                                </div>
+
+                            </div><!-- /.Button Contact -->
+
+                            <!-- Contact infos -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 info-contact no-padding no-margin">
+                                <h2><span class="telephone-bold">819 536-0777</span><a href="" class="contact-email">info@ccishawinigan.ca</a></h2>
+                                <p>1250, avenue de la Station, bureau 1-116<span>Shawinigan (Québec) G9N 1W8</span></p>
+                            </div>
+
+                        </section><!-- /.Section Promo Rabais -->
+
+                        <!-- Logo circle -->
+                        <div class="position-logo-ccis">
+
+                            <!-- image logo -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 logo-circle">
+                                <img src="img/img-circle-logo-ccis.png" alt="" class="img-responsive img-logo-ccis">
+                            </div>
+
+                        </div>
+
+                    </div><!-- /.Bloc Promo et Pub -->
+                </div><!-- /.row -->
+                
+            </div><!-- /.container -->
+        </div><!-- inner-top-shadow -->
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="/js/blogue.js"></script>
+	</body>
+</html>
