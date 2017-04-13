5fd564cbabe0d6a319672979530e275edd5c73e6
Dave <dave_bussiere@hotmail.com>
2017-04-12 21:11:00 -0400

    DONE // page Nous Joindre
    DONE // page Nous Joindre
    
    Voir fichiers : nous-joindre.php + nous-joindre.css
    
    - intégration statique, OK
    - fullscreen, OK
    - responsive, OK

---
+++ nous-joindre.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>NOUS JOINDRE</title>
+		<script src="https://use.fontawesome.com/663ece6c6a.js"></script>
+		
+		<!-- GOOGLE FONT -->
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/nous-joindre.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+        <!-- Top bar Section Finaliste -->
+		<div class="container-fluid no-padding no-margin">
+
+            <!-- Top Bar -->
+			<h2 class="top-bar-section nous-joindre">Nous joindre</h2>
+
+            <!-- Title Finaliste -->
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 title-nous-joindre">
+                <h3>Coordonnées</h3>
+            </div>
+
+		</div>
+
+        <!-- Section Adresse -->
+        <section class="section-address">
+            <div class="container">
+
+                <div class="row">
+
+                    <!-- Bloc Map + Adresse -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bloc-map-address no-padding no-margin">
+
+                        <!-- ONLY VISIBLE on MOBILE -->
+                        <a href="" class="view-on-google-map visible-mobile">voir sur Google Map</a>
+
+                        <!-- Map -->
+                        <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7 no-padding no-margin">
+                            <img src="" alt="" class="hidden-mobile img-responsive google-map">
+                        </div>
+                        
+                        <!-- Adresse -->
+                        <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 address no-padding no-margin">
+                            <p>1250, avenue de la Station, bureau 1-116</p>
+                            <p>Shawinigan (Québec)  G9N 1W8</p>
+                            <h3 class="address-top-padding">Téléphone : 819 536-0777</h3>
+                            <h3>Télécopieur : 819 536-0039</h3>
+                            <a href="">info@ccishawinigan.ca</a>
+                        </div>
+
+                    </div><!-- /.Bloc Google Map -->
+
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+
+            <!-- Section Title -->
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 section-question no-padding no-margin">
+                <h2>Une question en particulier?</h2>
+            </div>
+
+        </section><!-- /.Section Adresse -->
+
+        <!-- Contact -->
+        <div class="gray-background">
+            <div class="container">
+
+                <div class="row">
+
+                    <!-- Bloc CONTACT -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 no-padding no-margin bloc-img-dirigeant">
+
+                            <!-- Image -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                <img src="img/img-default-dirigeant.png" alt="" class="img-responsive img-dirigeant">
+                                <h3>M. Martin St-Pierre<span>directeur général de la CCIS</span></h3>
+                            </div>
+
+                            <!-- Button Contact -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-lg-push-3 bloc-email no-padding no-margin">
+                                
+                                <a href="" class="btn-email-contact">martin@ccishawinigan.ca</a>
+
+                            </div><!-- /.Button Contact -->
+
+                    </div><!-- /.Bloc CONTACT-->
+
+                    <!-- Bloc CONTACT -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 no-padding no-margin bloc-img-dirigeant">
+
+                            <!-- Image -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                <img src="img/img-default-dirigeant.png" alt="" class="img-responsive img-dirigeant">
+                                <h3>Mme. Claudine Drolet<span>Responsable des communications et des événements</span></h3>
+                            </div>
+
+                            <!-- Button Contact -->
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-lg-push-3 bloc-email no-padding no-margin">
+                                
+                                <a href="" class="btn-email-contact">claudine@ccishawinigan.ca</a>
+
+                            </div><!-- /.Button Contact -->
+
+                    </div><!-- /.Bloc CONTACT-->
+
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </div><!-- /.gray-background -->
+
+        <!-- Formulaire -->
+        <section class="formulaire">
+            <div class="container">
+                <div class="row">
+
+                    <!-- Formulaire -->
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bloc-formulaire">
+                        
+                        <form action="" method="POST" role="form">
+
+                            <!-- LEFT part Form -->
+                            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+
+                                <!-- Informations -->
+                                <legend class="col-xs-12 col-sm-12 col-md-12 col-lg-12">Informations</legend>
+
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+                                    <label for="" class="ccis-form-label" required="required">Prénom *</label>
+                                    <input type="text" class="ccis-form-input-text" id="" placeholder="">
+                                </div>
+                                
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+                                    <label for="" class="ccis-form-label" required="required">Nom *</label>
+                                    <input type="text" class="ccis-form-input-text" id="" placeholder="">
+                                </div>
+
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+                                    <label for="" class="ccis-form-label" required="required">Courriel *</label>
+                                    <input type="text" class="ccis-form-input-text" id="" placeholder="">
+                                </div>
+
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+                                    <label for="" class="ccis-form-label">Téléphone</label>
+                                    <input type="text" class="ccis-form-input-text" id="" placeholder="">
+                                </div>
+
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+                                    <label for="" class="ccis-form-label">Ville</label>
+                                    <input type="text" class="ccis-form-input-text" id="" placeholder="">
+                                </div>
+
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 no-padding no-margin">
+                                    <label for="" class="ccis-form-label">Code postal</label>
+                                    <input type="text" class="ccis-form-input-text" id="" placeholder="">
+                                </div>
+
+                            </div><!-- /.LEFT part Form -->
+
+                            <!-- RIGHT part Form -->
+                            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+
+                                <!-- Informations -->
+                                <legend class="col-xs-12 col-sm-12 col-md-12 col-lg-12">Commentaires</legend>
+
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bloc-commentaires no-padding no-margin">
+                                    <label for="" class="ccis-form-label">Laissez vos commentaires</label>
+                                    <textarea name="" id="" class="ccis-form-input-textarea" rows="3" required="required"></textarea>
+                                </div>
+
+                            </div><!-- /.RIGHT part Form -->
+
+                            <!-- Submit -->
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 bloc-submit no-padding no-margin">
+                                    <button type="submit" class="btn-ccis-submit">Envoyer</button>
+                                </div>
+                            </div><!-- /.Submit -->
+
+                        </form>
+                    </div><!-- /.Formulaire -->                    
+
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </section><!-- /.Formulaire -->
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="/js/blogue.js"></script>
+	</body>
+</html>
